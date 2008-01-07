ba1efed4180fac77d3aaf61e8c4d8781fa0f6739
Chris Mason <chris.mason@oracle.com>
2008-01-07 15:25:48 -0500

    btrfs_print_node: don't use btrfs_item_key_to_cpu on nodes

--- print-tree.c
+++ print-tree.c
@@ -227,7 +227,7 @@ void btrfs_print_tree(struct btrfs_root *root, struct extent_buffer *eb)
 	size = btrfs_level_size(root, btrfs_header_level(eb) - 1);
 	for (i = 0; i < nr; i++) {
 		u64 blocknr = btrfs_node_blockptr(eb, i);
-		btrfs_item_key_to_cpu(eb, &key, i);
+		btrfs_node_key_to_cpu(eb, &key, i);
 		printf("\tkey %d (%llu %x %llu) block %llu (%llu) gen %llu\n",
 		       i,
 		       (unsigned long long)key.objectid,
