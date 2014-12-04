48e728d1120c9e5482b5e4aa4dd91f071acfd6bb
Karel Zak <kzak@redhat.com>
2014-12-04 13:26:46 +0100

    btrfs-progs: autoconf: add ./configure script
    - add ./autogen.sh script (necessary after git clean/clone)
    - add ./configure.ac
    - copy autotool helper scripts from automake
    - modify version.sh to be usable from the configure script
    - rename Makefile to Makefile.in and use basic variables from configure.ac
    
    Signed-off-by: Karel Zak <kzak@redhat.com>
    Signed-off-by: David Sterba <dsterba@suse.cz>

---
+++ autogen.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+#
+# Helps generate autoconf stuff, when code is checked out from SCM.
+#
+# Copyright (C) 2006-2014 - Karel Zak <kzak@redhat.com>
+#
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+THEDIR=`pwd`
+cd $srcdir
+DIE=0
+
+test -f btrfs.c || {
+	echo
+	echo "You must run this script in the top-level btrfs-progs directory"
+	echo
+	DIE=1
+}
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to generate btrfs-progs build system."
+	echo
+	DIE=1
+}
+(autoheader --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoheader installed to generate btrfs-progs build system."
+	echo "The autoheader command is part of the GNU autoconf package."
+	echo
+	DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to generate btrfs-progs build system."
+	echo 
+	DIE=1
+}
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+echo
+echo "Generate build-system by:"
+echo "   aclocal:    $(aclocal --version | head -1)"
+echo "   autoconf:   $(autoconf --version | head -1)"
+echo "   autoheader: $(autoheader --version | head -1)"
+echo "   automake:   $(automake --version | head -1)"
+
+chmod +x version.sh
+rm -rf autom4te.cache
+
+aclocal $AL_OPTS
+autoconf $AC_OPTS
+autoheader $AH_OPTS
+
+# it's better to use helper files from automake installation than
+# maintain copies in git tree
+HELPER_DIR=$(automake --print-libdir)
+mkdir -p config/
+cp $HELPER_DIR/config.guess config/
+cp $HELPER_DIR/config.sub config/
+cp $HELPER_DIR/install-sh config/
+
+cd $THEDIR
+
+echo
+echo "Now type '$srcdir/configure' and 'make' to compile."
+echo
