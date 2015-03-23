bc400acbffad2bf1b0fa1e431260cd70dbe6fcb4
David Sterba <dsterba@suse.cz>
2015-03-23 18:45:56 +0100

    btrfs-progs: convert, add long options for all short options
    Signed-off-by: David Sterba <dsterba@suse.cz>

--- btrfs-convert.c
+++ btrfs-convert.c
@@ -2800,15 +2800,16 @@ fail:
 static void print_usage(void)
 {
 	printf("usage: btrfs-convert [options] device\n");
-	printf("\t-d             disable data checksum\n");
-	printf("\t-i             ignore xattrs and ACLs\n");
-	printf("\t-n             disable packing of small files\n");
-	printf("\t-N SIZE        set filesystem nodesize\n");
-	printf("\t-r             roll back to ext2fs\n");
-	printf("\t-l LABEL       set filesystem label\n");
-	printf("\t-L             use label from converted fs\n");
-	printf("\t-p             show converting progress (default)\n");
-	printf("\t--no-progress  show only overview, not the detailed progress\n");
+	printf("options:\n");
+	printf("\t-d|--no-datasum        disable data checksum, sets NODATASUM\n");
+	printf("\t-i|--no-xattr          ignore xattrs and ACLs\n");
+	printf("\t-n|--no-inline         disable inlining of small files to metadata\n");
+	printf("\t-N|--nodesize SIZE     set filesystem metadata nodesize\n");
+	printf("\t-r|--rollback          roll back to ext2fs\n");
+	printf("\t-l|--label LABEL       set filesystem label\n");
+	printf("\t-L|--copy-label        use label from converted filesystem\n");
+	printf("\t-p|--progress          show converting progress (default)\n");
+	printf("\t--no-progress          show only overview, not the detailed progress\n");
 }
 
 int main(int argc, char *argv[])
@@ -2832,6 +2833,14 @@ int main(int argc, char *argv[])
 		static const struct option long_options[] = {
 			{ "no-progress", no_argument, NULL,
 				GETOPT_VAL_NO_PROGRESS },
+			{ "no-datasum", no_argument, NULL, 'd' },
+			{ "no-inline", no_argument, NULL, 'n' },
+			{ "no-xattr", no_argument, NULL, 'i' },
+			{ "rollback", no_argument, NULL, 'r' },
+			{ "progress", no_argument, NULL, 'p' },
+			{ "label", required_argument, NULL, 'l' },
+			{ "copy-label", no_argument, NULL, 'L' },
+			{ "nodesize", required_argument, NULL, 'N' },
 			{ NULL, 0, NULL, 0 }
 		};
 		int c = getopt_long(argc, argv, "dinN:rl:Lp", long_options,
