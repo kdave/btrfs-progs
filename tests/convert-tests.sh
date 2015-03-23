d738c3d292135342c094c32d0ec03eed23842213
David Sterba <dsterba@suse.cz>
2015-03-23 16:57:03 +0100

    btrfs-progs: convert tests: add remaining supported nodesizes
    That's 8k, 32k and 64k.
    
    Signed-off-by: David Sterba <dsterba@suse.cz>

--- tests/convert-tests.sh
+++ tests/convert-tests.sh
@@ -37,6 +37,15 @@ test(){
 test "ext2 4k nodesize" 4096 mke2fs -b 4096
 test "ext3 4k nodesize" 4096 mke2fs -j -b 4096
 test "ext4 4k nodesize" 4096 mke2fs -t ext4 -b 4096
+test "ext2 8k nodesize" 8192 mke2fs -b 4096
+test "ext3 8k nodesize" 8192 mke2fs -j -b 4096
+test "ext4 8k nodesize" 8192 mke2fs -t ext4 -b 4096
 test "ext2 16k nodesize" 16384 mke2fs -b 4096
 test "ext3 16k nodesize" 16384 mke2fs -j -b 4096
 test "ext4 16k nodesize" 16384 mke2fs -t ext4 -b 4096
+test "ext2 32k nodesize" 32768 mke2fs -b 4096
+test "ext3 32k nodesize" 32768 mke2fs -j -b 4096
+test "ext4 32k nodesize" 32768 mke2fs -t ext4 -b 4096
+test "ext2 64k nodesize" 65536 mke2fs -b 4096
+test "ext3 64k nodesize" 65536 mke2fs -j -b 4096
+test "ext4 64k nodesize" 65536 mke2fs -t ext4 -b 4096
