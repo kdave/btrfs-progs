02abd61aa0566cde9e3a324cb207241e2259564d
Zhao Lei <zhaolei@cn.fujitsu.com>
2015-07-27 20:24:32 +0800

    btrfs-progs: Introduce a misc test for thread conflict in btrfs-convert
    Current code of btrfs-convert have a bug of thread conflict, which caused
    invalid memory accessing between threads, and make program panic.
    
    This patch add a test item for above bug, as:
     # ./misc-tests.sh
        [TEST]   001-btrfstune-features
        [TEST]   002-uuid-rewrite
        [TEST]   003-zero-log
        [TEST]   004-convert-thread-conflict
     failed: btrfs-convert /root/btrfsprogs/tests/test.img
     test failed for case 004-convert-thread-conflict
     #
     # cat misc-tests-results.txt
     ...
     ############### btrfs-convert /root/btrfsprogs/tests/test.img
     trans 7 running 5
     ctree.c:363: btrfs_cow_block: Assertion `1` failed.
     btrfs-convert(btrfs_cow_block+0x92)[0x40acaf]
     btrfs-convert(btrfs_search_slot+0x1cb)[0x40c50f]
     btrfs-convert(btrfs_csum_file_block+0x20f)[0x41d83a]
     btrfs-convert[0x43422d]
     btrfs-convert[0x4342cd]
     btrfs-convert[0x4345ca]
     btrfs-convert[0x434767]
     btrfs-convert[0x435770]
     btrfs-convert[0x439748]
     btrfs-convert(main+0x13f8)[0x43b09d]
     /lib64/libc.so.6(__libc_start_main+0xfd)[0x335e01ecdd]
     btrfs-convert[0x407649]
     create btrfs filesystem:
             blocksize: 4096
             nodesize:  16384
             features:  extref, skinny-metadata (default)
     creating btrfs metadata.
    
     creating ext2fs image file.
     failed: btrfs-convert /root/btrfsprogs/tests/test.img
     test failed for case 004-convert-thread-conflict
     #
    
    Note that this bug is not happened every time, especilly in slow
    device as loop(slow cpu with fast block device is likely to trigger).
    I set loop count to 20 to make bug happened in 90% tests.
    
    Suggested-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Zhao Lei <zhaolei@cn.fujitsu.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

---
+++ tests/misc-tests/005-convert-progress-thread-crash/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# test convert-thread-conflict
+
+source $TOP/tests/common
+
+check_prereq btrfs
+mkfs.ext4 -V &>/dev/null || _not_run "mkfs.ext4 not found"
+prepare_test_dev 1G
+
+for ((i = 0; i < 20; i++)); do
+	echo "loop $i" >>$RESULTS
+	mkfs.ext4 -F "$TEST_DEV" &>>$RESULTS || _not_run "mkfs.ext4 failed"
+	run_check $TOP/btrfs-convert "$TEST_DEV"
+done
