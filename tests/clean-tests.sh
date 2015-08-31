107adbd05d037567d20211732e5c9039853319ed
Zhao Lei <zhaolei@cn.fujitsu.com>
2015-08-31 13:04:38 +0800

    btrfs-progs: tests: umount TEST_MNT in clean-tests.sh
    If a testcase failed, we can't run it(or other tests needs mount) again,
      # ./misc-tests.sh 007
       [TEST]   007-subvolume-sync
       failed: fail
       test failed for case 007-subvolume-sync
      # ./misc-tests.sh 007
       [TEST]   007-subvolume-sync
       failed: mount /root/btrfs-progs/tests/test.img /root/btrfs-progs/tests/mnt
       test failed for case 007-subvolume-sync
    
    This patch add "umount $TEST_MNT" to clean-tests.sh, to let user
    clean mountpoint easily.
    
    After patch:
      # ./misc-tests.sh  007
       [TEST]   007-subvolume-sync
       failed: fail
       test failed for case 007-subvolume-sync
      #
      # clean-tests.sh
      #
      # ./misc-tests.sh  007
       [TEST]   007-subvolume-sync
       failed: fail
       test failed for case 007-subvolume-sync
    
    Suggested-by: David Sterba <dsterba@suse.cz>
    Signed-off-by: Zhao Lei <zhaolei@cn.fujitsu.com>
    [added sudo helper to umount]
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/clean-tests.sh
+++ tests/clean-tests.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 # remove all intermediate files from tests
 
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+TOP=$(readlink -f $SCRIPT_DIR/../)
+source $TOP/tests/common
+
+setup_root_helper
+
 if [ "$BUILD_VERBOSE" = 1 ]; then
 	verbose=-print
 fi
 
-SCRIPT_DIR=$(dirname $(readlink -f $0))
-TOP=$(readlink -f $SCRIPT_DIR/../)
+$SUDO_HELPER umount "$TEST_MNT" &>/dev/null
 
 if ! cd $TOP/tests; then
 	echo "ERROR: cannot cd to $TOP/tests"
