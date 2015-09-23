9e0c8e148d5d7240e540f825546a434a24ac2660
Zhao Lei <zhaolei@cn.fujitsu.com>
2015-09-23 15:19:06 +0800

    btrfs-progs: tests: Introduce misc-tests/008-leaf-crossing-stripes
    To check is btrfs-convert create bad filesystem with
    leaf across stripes.
    
    It is happened in progs version <=v4.1.2, and fixed by patch titled:
    btrfs: convert: Avoid allocating metadata extent crossing stripe boundary
    which was merged in v4.2.
    
    Notice thar this testcase can not report error in old version of
    btrfs-progs, because "btrfs check" can't check this type of error
    in those version, but we have another testcase in fsck-tests, to
    check is "btrfs check" support this check.
    
    So, the above 2 testcase together can check leaf-crossing-stripes
    bug in all versions.
    
    Signed-off-by: Zhao Lei <zhaolei@cn.fujitsu.com>
    [renamed and other minor changes]
    Signed-off-by: David Sterba <dsterba@suse.com>

---
+++ tests/misc-tests/008-leaf-crossing-stripes/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# test if btrfs-convert creates a filesystem without leaf crossing stripes
+
+source $TOP/tests/common
+
+check_prereq btrfs-convert
+check_prereq btrfs
+
+# In my test, it happened in 514M~560M, 737M~769M, 929M~917M,
+# and HAVE_ERROR=((size + 1) / 2) % 2 if size >= 970
+#
+SIZE_FROM=514
+SIZE_END=560
+A_PRIME_NUM=17
+for ((size = SIZE_FROM; size <= SIZE_END; size += A_PRIME_NUM)); do
+	run_check truncate -s "$size"M "$IMAGE"
+	run_check mkfs.ext4 -F "$IMAGE"
+	run_check $TOP/btrfs-convert "$IMAGE"
+	run_check_stdout $TOP/btrfs check "$IMAGE" 2>&1 |
+		grep -q "crossing stripe boundary" &&
+		_fail "leaf crossing stripes after btrfs-convert"
+done
+
+# grep will expectedly fail
+exit 0
