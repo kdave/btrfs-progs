5f77daa6a1673b130a566130144c80ce86ed1d0d
Zhao Lei <zhaolei@cn.fujitsu.com>
2015-09-23 15:19:05 +0800

    btrfs-progs: tests: Introduce fsck-tests/018-leaf-crossing-stripes
    To test if fsck can check detec "leaf crossing stripes".
    
    This function was introduced from patch titled:
    btrfs-progs: fsck: Check if a metadata tree block crossing stripe boundary
    
    Signed-off-by: Zhao Lei <zhaolei@cn.fujitsu.com>
    [renamed and other minor changes]
    Signed-off-by: David Sterba <dsterba@suse.com>

---
+++ tests/fsck-tests/018-leaf-crossing-stripes/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source $TOP/tests/common
+
+check_prereq btrfs
+
+image=$(extract_image "./default_case.raw.xz")
+run_check_stdout $TOP/btrfs check "$image" 2>&1 |
+	grep -q "crossing stripe boundary" ||
+	_fail "no expected error message in the output"
+
+rm -f "$image"
