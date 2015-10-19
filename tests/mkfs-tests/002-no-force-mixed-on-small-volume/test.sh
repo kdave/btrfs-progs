1e92cd0d9bc0d9ed8efce639cef0b552901c2cb4
David Sterba <dsterba@suse.com>
2015-10-19 18:12:35 +0200

    btrfs-progs: tests: add 002-no-force-mixed-on-small-volume
    Verify that we do not force mixed block groups on small volumes anymore.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

---
+++ tests/mkfs-tests/002-no-force-mixed-on-small-volume/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Verify that we do not force mixed block groups on small volumes anymore
+
+source $TOP/tests/common
+
+check_prereq btrfs-show-super
+check_prereq mkfs.btrfs
+setup_root_helper
+
+run_check truncate -s 512M $IMAGE
+mixed=$(run_check_stdout $TOP/mkfs.btrfs -n 64k -f $IMAGE | egrep 'Data|Metadata')
+echo "$mixed" | grep -q -v 'Data+Metadata:' || _fail "unexpected: created a mixed-bg filesystem"
