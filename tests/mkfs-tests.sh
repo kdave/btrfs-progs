liubogithub/btrfs-progs-1

btrfs-progs: tests: print test type
Makes it a bit more clear:

    [TEST/conv]   ext4 32k nodesize, btrfs no-holes
    [TEST/conv]   ext2 64k nodesize, btrfs no-holes
    [TEST/conv]   ext3 64k nodesize, btrfs no-holes
    [TEST/conv]   ext4 64k nodesize, btrfs no-holes
    [TEST]        misc-tests.sh
    [TEST/misc]   001-btrfstune-features
    [TEST/misc]   002-uuid-rewrite
    [TEST/misc]   003-zero-log
    [TEST/misc]   004-shrink-fs
    [TEST/misc]   005-convert-progress-thread-crash
    [TEST/misc]   006-image-on-missing-device
    [TEST/misc]   007-subvolume-sync
    [TEST/misc]   008-leaf-crossing-stripes
    [TEST/misc]   009-subvolume-sync-must-wait
    [TEST/misc]   010-convert-delete-ext2-subvol
    [TEST/misc]   011-delete-missing-device
    [TEST]        fuzz-tests.sh
    [TEST/fuzz]   001-simple-unmounted

Signed-off-by: David Sterba <dsterba@suse.com>

tests/mkfs-tests.sh
@@ -31,7 +31,7 @@ check_prereq btrfs
 for i in $(find $TOP/tests/mkfs-tests -maxdepth 1 -mindepth 1 -type d	\
 	${TEST:+-name "$TEST"} | sort)
 do
-	echo "    [TEST]   $(basename $i)"
+	echo "    [TEST/misc]   $(basename $i)"
 	cd $i
 	echo "=== Entering $i" >> $RESULTS
 	if [ -x test.sh ]; then

kdave/btrfs-progs

tests/mkfs-tests.sh
@@ -31,7 +31,7 @@ check_prereq btrfs
 for i in $(find $TOP/tests/mkfs-tests -maxdepth 1 -mindepth 1 -type d	\
 	${TEST:+-name "$TEST"} | sort)
 do
-	echo "    [TEST]   $(basename $i)"
+	echo "    [TEST/mkfs]   $(basename $i)"
 	cd $i
 	echo "=== Entering $i" >> $RESULTS
 	if [ -x test.sh ]; then