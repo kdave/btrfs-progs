fde5ae2e49a9cd7091fb8db8d748d7687a544d23
Qu Wenruo <quwenruo@cn.fujitsu.com>
2016-06-03 10:34:25 +0800

    btrfs-progs: convert-tests: Add support for custom test scripts
    Add support for custom convert test scripts, just like fsck tests.
    
    Instead of generic convert tests, we need more specifically created images
    for new convert tests.
    
    This patch provide the needed infrastructure for later convert test
    cases.
    
    Signed-off-by: Qu Wenruo <quwenruo@cn.fujitsu.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/convert-tests.sh
+++ tests/convert-tests.sh
@@ -15,12 +15,36 @@ DATASET_SIZE=50
 
 source $TOP/tests/common
 
+# Allow child test to use $TOP and $RESULTS
+export TOP
+export RESULTS
+export LANG
+
 rm -f $RESULTS
 
 setup_root_helper
 prepare_test_dev 512M
 
 CHECKSUMTMP=$(mktemp --tmpdir btrfs-progs-convert.XXXXXXXXXX)
+
+run_one_test() {
+	local testname
+
+	testname="$1"
+	echo "    [TEST/conv]   $testname"
+	cd $testname
+	echo "=== Entering $testname" >> $RESULTS
+	if [ -x test.sh ]; then
+		# Difference convert test case needs different tools to restore
+		# and check image, so only support custom test scripts
+		./test.sh
+		if [ $? -ne 0 ]; then
+			_fail "test failed for case $(basename $testname)"
+		fi
+	else
+		_fail "custom test script not found"
+	fi
+}
 
 generate_dataset() {
 
@@ -170,6 +194,13 @@ for feature in '' 'extref' 'skinny-metadata' 'no-holes'; do
 	convert_test "$feature" "ext2 64k nodesize" 65536 mke2fs -b 4096
 	convert_test "$feature" "ext3 64k nodesize" 65536 mke2fs -j -b 4096
 	convert_test "$feature" "ext4 64k nodesize" 65536 mke2fs -t ext4 -b 4096
+done
+
+# Test special images
+for i in $(find $TOP/tests/convert-tests -maxdepth 1 -mindepth 1 -type d \
+	   ${TEST:+-name "$TEST"} | sort)
+do
+	run_one_test "$i"
 done
 
 rm $CHECKSUMTMP
