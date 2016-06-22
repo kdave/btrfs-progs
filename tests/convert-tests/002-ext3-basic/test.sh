cd9b35c37ae03d8ea7cc35c4ffe501925e75e629
David Sterba <dsterba@suse.com>
2016-06-22 15:52:17 +0200

    btrfs-progs: tests: fix filesytem type creation for convert tests
    The extN filesystem type was lost when the separate tests were created
    and we've been testing only ext2. The tests pass for ext3 and ext4
    though.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/convert-tests/002-ext3-basic/test.sh
+++ tests/convert-tests/002-ext3-basic/test.sh
@@ -8,9 +8,9 @@ prepare_test_dev 512M
 check_prereq btrfs-convert
 
 for feature in '' 'extref' 'skinny-metadata' 'no-holes'; do
-	convert_test "$feature" "ext3 4k nodesize" 4096 mke2fs -b 4096
-	convert_test "$feature" "ext3 8k nodesize" 8192 mke2fs -b 4096
-	convert_test "$feature" "ext3 16k nodesize" 16384 mke2fs -b 4096
-	convert_test "$feature" "ext3 32k nodesize" 32768 mke2fs -b 4096
-	convert_test "$feature" "ext3 64k nodesize" 65536 mke2fs -b 4096
+	convert_test "$feature" "ext3 4k nodesize" 4096 mke2fs -j -b 4096
+	convert_test "$feature" "ext3 8k nodesize" 8192 mke2fs -j -b 4096
+	convert_test "$feature" "ext3 16k nodesize" 16384 mke2fs -j -b 4096
+	convert_test "$feature" "ext3 32k nodesize" 32768 mke2fs -j -b 4096
+	convert_test "$feature" "ext3 64k nodesize" 65536 mke2fs -j -b 4096
 done
