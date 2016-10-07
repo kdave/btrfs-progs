9119319ef99ffd742349f4b91aa8caab02d38fc9
Qu Wenruo <quwenruo@cn.fujitsu.com>
2016-10-07 15:22:59 +0800

    btrfs-progs: test: Add test image for btrfsck qgroup rescan detection
    Fixed by commit 7c646c538e74 btrfs-progs: qgroup: Fix regression leads
    to corrupted qgroup status.
    
    Add minimal test image for that fix.
    
    Signed-off-by: Qu Wenruo <quwenruo@cn.fujitsu.com>
    [ minor cleanups in test.sh ]
    Signed-off-by: David Sterba <dsterba@suse.com>

---
+++ tests/fsck-tests/022-qgroup-rescan-halfway/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# check whether btrfsck can detect running qgroup rescan
+
+source $TOP/tests/common
+
+check_prereq btrfs
+
+check_image() {
+	local image
+
+	image=$1
+	run_check_stdout $TOP/btrfs check "$image" 2>&1 | \
+		grep -q "Counts for qgroup id"
+	if [ $? -eq 0 ]; then
+		_fail "Btrfs check doesn't detect rescan correctly"
+	fi
+}
+
+check_all_images "."
