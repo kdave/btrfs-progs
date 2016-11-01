e244550b42399370c11a4685a1da1954ec8c269e
David Sterba <dsterba@suse.com>
2016-11-01 13:32:26 +0100

    btrfs-progs: introduce key type for persistent permanent items
    The number of distinct key types is not that big that we could waste one
    for something new we want to store in the tree.
    
    Similar to the temporary items, we'll introduce a new name for an
    existing key value and use the objectid for further extension.  The
    victim is the BTRFS_DEV_STATS_KEY (248).
    
    The device stats are an example of a permanent item.
    
    [ kernel patch 50c2d5abe64c1726b48d292a2ab04f60e8238933 ]
    Signed-off-by: David Sterba <dsterba@suse.com>

--- ctree.h
+++ ctree.h
@@ -1229,10 +1229,24 @@ struct btrfs_root {
 #define BTRFS_TEMPORARY_ITEM_KEY	248
 
 /*
- * Persistently stores the io stats in the device tree.
- * One key for all stats, (0, BTRFS_DEV_STATS_KEY, devid).
+ * Obsolete name, see BTRFS_PERSISTENT_ITEM_KEY
  */
-#define BTRFS_DEV_STATS_KEY	249
+#define BTRFS_DEV_STATS_KEY		249
+
+/*
+ * The key type for tree items that are stored persistently and usually exist
+ * for a long period, eg. filesystem lifetime. The item kinds can be status
+ * information, stats or preference values. The item can exist in any tree.
+ *
+ * [subtype, BTRFS_PERSISTENT_ITEM_KEY, data]
+ *
+ * Existing items:
+ *
+ * - device statistics, store IO stats in the device tree, one key for all
+ *   stats
+ *   (0, BTRFS_DEV_STATS_KEY, 0)
+ */
+#define BTRFS_PERSISTENT_ITEM_KEY	249
 
 /*
  * Persistently stores the device replace state in the device tree.
