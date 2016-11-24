kdave/btrfs-progs

commit 1c85c3de5aab997ff66ea95cb0f2c9f79726ec40

btrfs-progs: use existing rootid resolving helper in btrfs_list_get_path_rootid
The utils helper is not verbose in case of an error, for now the helper
used for subvolume listing will print the error message but not
duplicate the ioctl anymore.

Signed-off-by: David Sterba <dsterba@suse.com>

utils.c
@@ -4161,10 +4161,8 @@ int get_subvol_info(const char *fullpath, struct root_info *get_ri)
 		goto out;
 
 	ret = btrfs_list_get_path_rootid(fd, &sv_id);
-	if (ret) {
-		error("can't get rootid for '%s'", fullpath);
+	if (ret)
 		goto out;
-	}
 
 	mntfd = btrfs_open_dir(mnt, &dirstream2, 1);
 	if (mntfd < 0)

btrfs-list.c
@@ -1888,19 +1888,12 @@ int btrfs_list_parse_filter_string(char *opt_arg,
 
 int btrfs_list_get_path_rootid(int fd, u64 *treeid)
 {
-	int  ret;
-	struct btrfs_ioctl_ino_lookup_args args;
+	int ret;
 
-	memset(&args, 0, sizeof(args));
-	args.objectid = BTRFS_FIRST_FREE_OBJECTID;
-
-	ret = ioctl(fd, BTRFS_IOC_INO_LOOKUP, &args);
-	if (ret < 0) {
-		fprintf(stderr,
-			"ERROR: can't perform the search - %s\n",
+	ret = lookup_path_rootid(fd, treeid);
+	if (ret < 0)
+		error("cannot resolve rootid for path: %s",
 			strerror(errno));
-		return ret;
-	}
-	*treeid = args.treeid;
-	return 0;
+
+	return ret;
 }

cmds-receive.c
@@ -1106,11 +1106,8 @@ static int do_receive(struct btrfs_receive *r, const char *tomnt,
 	 * subvols we want to receive in.
 	 */
 	ret = btrfs_list_get_path_rootid(r->mnt_fd, &subvol_id);
-	if (ret) {
-		error("cannot resolve our subvolid: %d",
-			ret);
+	if (ret)
 		goto out;
-	}
 
 	root_subvol_path[0] = 0;
 	ret = btrfs_subvolid_resolve(r->mnt_fd, root_subvol_path,

cmds-subvolume.c
@@ -548,10 +548,8 @@ static int cmd_subvol_list(int argc, char **argv)
 	}
 
 	ret = btrfs_list_get_path_rootid(fd, &top_id);
-	if (ret) {
-		error("can't get rootid for '%s'", subvol);
+	if (ret)
 		goto out;
-	}
 
 	if (is_list_all)
 		btrfs_list_setup_filter(&filter_set,

commit e5362f5feb6b526ff3145bb0d7ce164e4d501edd

btrfs-progs: move lookup_path_rootid to library utils
The refactoring in commit 1c85c3de5aab997ff66ea95cb0f2c9f79726ec40 has
broken use of libbtrfs that does not exhibit during build but at the run
time.

Fixes: 1c85c3de5aab997ff66ea95cb0f2c9f79726ec40
Bugzilla: https://bugs.gentoo.org/show_bug.cgi?id=600078
Reported-by: Mike Gilbert <floppymaster@gmail.com>
Signed-off-by: David Sterba <dsterba@suse.com>

utils-lib.c
@@ -2,9 +2,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <sys/ioctl.h>
+#include <ioctl.h>
 
 #if BTRFS_FLAT_INCLUDES
+#include "ctree.h"
 #else
+#include <btrfs/ctree.h>
 #endif /* BTRFS_FLAT_INCLUDES */
 
 /*
@@ -38,3 +42,29 @@ u64 arg_strtou64(const char *str)
 	}
 	return value;
 }
+
+/*
+ * For a given:
+ * - file or directory return the containing tree root id
+ * - subvolume return its own tree id
+ * - BTRFS_EMPTY_SUBVOL_DIR_OBJECTID (directory with ino == 2) the result is
+ *   undefined and function returns -1
+ */
+int lookup_path_rootid(int fd, u64 *rootid)
+{
+	struct btrfs_ioctl_ino_lookup_args args;
+	int ret;
+
+	memset(&args, 0, sizeof(args));
+	args.treeid = 0;
+	args.objectid = BTRFS_FIRST_FREE_OBJECTID;
+
+	ret = ioctl(fd, BTRFS_IOC_INO_LOOKUP, &args);
+	if (ret < 0)
+		return -errno;
+
+	*rootid = args.treeid;
+
+	return 0;
+}
+

utils.c
@@ -3647,31 +3647,6 @@ int ask_user(const char *question)
 }
 
 /*
- * For a given:
- * - file or directory return the containing tree root id
- * - subvolume return its own tree id
- * - BTRFS_EMPTY_SUBVOL_DIR_OBJECTID (directory with ino == 2) the result is
- *   undefined and function returns -1
- */
-int lookup_path_rootid(int fd, u64 *rootid)
-{
-	struct btrfs_ioctl_ino_lookup_args args;
-	int ret;
-
-	memset(&args, 0, sizeof(args));
-	args.treeid = 0;
-	args.objectid = BTRFS_FIRST_FREE_OBJECTID;
-
-	ret = ioctl(fd, BTRFS_IOC_INO_LOOKUP, &args);
-	if (ret < 0)
-		return -errno;
-
-	*rootid = args.treeid;
-
-	return 0;
-}
-
-/*
  * return 0 if a btrfs mount point is found
  * return 1 if a mount point is found but not btrfs
  * return <0 if something goes wrong