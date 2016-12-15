55e3a601951302641c4fd5e148f46449acc449c2
Tsutomu Itoh <t-itoh@jp.fujitsu.com>
2016-12-15 13:33:05 +0900

    btrfs-progs: tests: add test for --sync option of qgroup show
    Simple test script for the following patch.
    
       btrfs-progs: qgroup: add sync option to 'qgroup show'
    
    Signed-off-by: Tsutomu Itoh <t-itoh@jp.fujitsu.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

---
+++ tests/cli-tests/006-qgroup-show-sync/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# simple test of qgroup show --sync option
+
+source "$TOP/tests/common"
+
+check_prereq mkfs.btrfs
+check_prereq btrfs
+
+setup_root_helper
+prepare_test_dev 1g
+
+run_check "$TOP/mkfs.btrfs" -f "$IMAGE"
+run_check_mount_test_dev
+
+run_check $SUDO_HELPER "$TOP/btrfs" subvolume create "$TEST_MNT/Sub"
+run_check $SUDO_HELPER "$TOP/btrfs" quota enable "$TEST_MNT/Sub"
+
+for opt in '' '--' '--sync'; do
+	run_check $SUDO_HELPER "$TOP/btrfs" qgroup limit 300M "$TEST_MNT/Sub"
+	run_check $SUDO_HELPER dd if=/dev/zero of="$TEST_MNT/Sub/file" bs=1M count=200
+
+	run_check $SUDO_HELPER "$TOP/btrfs" qgroup show -re $opt "$TEST_MNT/Sub"
+
+	run_check $SUDO_HELPER "$TOP/btrfs" qgroup limit none "$TEST_MNT/Sub"
+	run_check $SUDO_HELPER rm -f "$TEST_MNT/Sub/file"
+	run_check "$TOP/btrfs" filesystem sync "$TEST_MNT/Sub"
+done
+
+run_check_umount_test_dev
