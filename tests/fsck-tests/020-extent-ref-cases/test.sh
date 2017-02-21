6203e63672ab34f401083957f580f5e9dce6a792
Qu Wenruo <quwenruo@cn.fujitsu.com>
2017-02-21 16:34:34 +0800

    btrfs-progs: tests: Add test image for lowmem mode block group false alert
    Add a minimal image which can reproduce the block group used space
    false alert for lowmem mode fsck.
    
    Reported-by: Christoph Anton Mitterer <calestyo@scientia.net>
    Signed-off-by: Qu Wenruo <quwenruo@cn.fujitsu.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/fsck-tests/020-extent-ref-cases/test.sh
+++ tests/fsck-tests/020-extent-ref-cases/test.sh
@@ -8,16 +8,23 @@
 # * shared_data_ref
 # * no_inline_ref (a extent item without inline ref)
 # * no_skinny_ref
+#
+# Special check for lowmem regression
+# * block_group_item_false_alert
+#   Containing a block group and its first extent at
+#   the beginning of leaf.
+#   Which caused false alert for lowmem mode.
 
 source $TOP/tests/common
 
 check_prereq btrfs
 
-for img in *.img
+for img in *.img *.raw.xz
 do
 	image=$(extract_image $img)
-	run_check_stdout $TOP/btrfs check "$image" 2>&1 |
-		grep -q "Errors found in extent allocation tree or chunk allocation" &&
-		_fail "unexpected error occurred when checking $img"
+
+	# Since the return value bug is already fixed, we don't need
+	# the old grep hack to detect bug.
+	run_check $TOP/btrfs check "$image"
 	rm -f "$image"
 done
