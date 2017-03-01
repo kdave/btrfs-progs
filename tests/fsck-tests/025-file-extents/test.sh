e14c15b19e1d6551fa1196581fd1ad672de441fe
David Sterba <dsterba@suse.com>
2017-03-01 17:10:44 +0100

    btrfs-progs: tests: fix permissions of temporary files in 025-file-extents
    If the tests are started from non-root user, the fallocate and xfs_io
    fail. Use the root helper as a workaround, we'd should fix the perms
    instead.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/fsck-tests/025-file-extents/test.sh
+++ tests/fsck-tests/025-file-extents/test.sh
@@ -18,9 +18,9 @@ test_paritical_write_into_prealloc()
 	run_check $SUDO_HELPER "$TOP/mkfs.btrfs" -f "$TEST_DEV"
 	run_check_mount_test_dev
 
-	run_check fallocate -l 128K "$TEST_MNT/file"
+	run_check $SUDO_HELPER fallocate -l 128K "$TEST_MNT/file"
 	sync
-	run_check xfs_io -c "pwrite 0 64K" "$TEST_MNT/file"
+	run_check $SUDO_HELPER xfs_io -c "pwrite 0 64K" "$TEST_MNT/file"
 	run_check_umount_test_dev
 	run_check "$TOP/btrfs" check "$TEST_DEV"
 }
@@ -33,7 +33,7 @@ test_compressed_inline_extent()
 	run_check $SUDO_HELPER "$TOP/mkfs.btrfs" -f "$TEST_DEV"
 	run_check_mount_test_dev -o compress=lzo,max_inline=2048
 
-	run_check xfs_io -f -c "pwrite 0 1K" "$TEST_MNT/file"
+	run_check $SUDO_HELPER xfs_io -f -c "pwrite 0 1K" "$TEST_MNT/file"
 	run_check_umount_test_dev
 	run_check "$TOP/btrfs" check "$TEST_DEV"
 }
