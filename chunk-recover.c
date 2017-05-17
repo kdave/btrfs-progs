c4893f15cedc59c14656b06b83bb5c8a441efe66
Qu Wenruo <quwenruo@cn.fujitsu.com>
2017-05-17 16:57:35 +0800

    btrfs-progs: Introduce sectorsize nodesize and stripesize members for btrfs_fs_info
    Just like what we do in kernel, since we will not support different
    leaf/node/stripe size per tree, there is no need to store these block
    sizes in btrfs_root.
    
    This patch will introduce these block size members into btrfs_fs_info
    structure, allowing us to convert such usage in later patches.
    
    Signed-off-by: Qu Wenruo <quwenruo@cn.fujitsu.com>

--- chunk-recover.c
+++ chunk-recover.c
@@ -1475,6 +1475,9 @@ open_ctree_with_broken_chunk(struct recover_control *rc)
 	}
 
 	memcpy(fs_info->fsid, &disk_super->fsid, BTRFS_FSID_SIZE);
+	fs_info->sectorsize = btrfs_super_sectorsize(disk_super);
+	fs_info->nodesize = btrfs_super_nodesize(disk_super);
+	fs_info->stripesize = btrfs_super_stripesize(disk_super);
 
 	ret = btrfs_check_fs_compatibility(disk_super, OPEN_CTREE_WRITES);
 	if (ret)
