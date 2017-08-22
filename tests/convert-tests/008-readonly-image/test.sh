0cd4485178dcf97f2ed58a59f5d3ade56aa11eb3
Jeff Mahoney <jeffm@suse.com>
2017-08-22 19:44:44 +0200

    btrfs-progs: tests: fix typo in convert-tests/008-readonly-image
    The dd in convert-tests/008-readonly-image is expected to fail, so
    there being a typo in the file name has gone unnoticed.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/convert-tests/008-readonly-image/test.sh
+++ tests/convert-tests/008-readonly-image/test.sh
@@ -16,10 +16,10 @@ convert_test_do_convert
 run_check_mount_test_dev
 
 # It's expected to fail
-$SUDO_HELPER dd if=/dev/zero of="$TEST_MNT/ext2_save/image" bs=1M count=1 \
+$SUDO_HELPER dd if=/dev/zero of="$TEST_MNT/ext2_saved/image" bs=1M count=1 \
 	&> /dev/null
 if [ $? -ne 1 ]; then
-	echo "after convert ext2_save/image is not read-only"
+	echo "after convert ext2_saved/image is not read-only"
 	exit 1
 fi
 run_check_umount_test_dev
