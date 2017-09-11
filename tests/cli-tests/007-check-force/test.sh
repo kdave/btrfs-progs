4a23dde9986272421da9fe44f553c441adefd4c8
David Sterba <dsterba@suse.com>
2017-09-11 17:29:03 +0200

    btrfs-progs: tests: fix run_mustfail in cli-tests/007-check-force
    The sanity check in run_mustfail does not work as expected and allowed
    the command to continue without the missing parameter
    
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=196893
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/cli-tests/007-check-force/test.sh
+++ tests/cli-tests/007-check-force/test.sh
@@ -12,10 +12,13 @@ prepare_test_dev
 
 run_check "$TOP/mkfs.btrfs" -f "$TEST_DEV"
 run_check_mount_test_dev
-run_mustfail $SUDO_HELPER "$TOP/btrfs" check "$TEST_DEV"
+run_mustfail "checking mounted filesystem without --force" \
+	$SUDO_HELPER "$TOP/btrfs" check "$TEST_DEV"
 run_check $SUDO_HELPER "$TOP/btrfs" check --force "$TEST_DEV"
-run_mustfail $SUDO_HELPER "$TOP/btrfs" check --force --repair "$TEST_DEV"
+run_mustfail "checking mounted filesystem with --force --repair" \
+	$SUDO_HELPER "$TOP/btrfs" check --force --repair "$TEST_DEV"
 run_check_umount_test_dev
 run_check $SUDO_HELPER "$TOP/btrfs" check "$TEST_DEV"
 run_check $SUDO_HELPER "$TOP/btrfs" check --force "$TEST_DEV"
-run_mustfail $SUDO_HELPER "$TOP/btrfs" check --force --repair "$TEST_DEV"
+run_mustfail "--force --repair on unmounted filesystem" \
+	$SUDO_HELPER "$TOP/btrfs" check --force --repair "$TEST_DEV"
