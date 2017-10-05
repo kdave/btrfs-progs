1d4224e4aab14f4967e54f96860f544072cb0442
David Sterba <dsterba@suse.com>
2017-10-05 16:25:21 +0200

    btrfs-progs: tests: don't list toplevel subvolme in 'subvol list'
    Signed-off-by: David Sterba <dsterba@suse.com>

---
+++ tests/misc-tests/027-subvol-list-deleted-toplevel/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# check that the toplevel subvolume is not listed as regular or deleted
+
+source "$TOP/tests/common"
+
+check_prereq mkfs.btrfs
+check_prereq btrfs
+
+prepare_test_dev
+
+run_check "$TOP/mkfs.btrfs" -f "$TEST_DEV"
+run_check_mount_test_dev
+run_check_stdout $SUDO_HELPER "$TOP/btrfs" subvolume list "$TEST_MNT" |
+	grep -i -q "id 5" && _fail "found toplevel among regular"
+run_check_stdout $SUDO_HELPER "$TOP/btrfs" subvolume list -d "$TEST_MNT" |
+	grep -i -q "id 5.*DELETED" && _fail "found toplevel among deleted"
+
+run_check_umount_test_dev
