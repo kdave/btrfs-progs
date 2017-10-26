kdave/btrfs-progs

commit 9fcdf8f8945bfddb7e1155bc9da68aed33e4737c

btrfs-progs: don't write to optarg in btrfs_qgroup_parse_sort_string
Dup the string that gets passed to strtok so we don't destroy callers
buffer.

Signed-off-by: David Sterba <dsterba@suse.com>

qgroup.c
@@ -1233,8 +1233,14 @@ int btrfs_qgroup_parse_sort_string(char *opt_arg,
 	char *p;
 	char **ptr_argv;
 	int what_to_sort;
+	char *opt_tmp;
+	int ret = 0;
 
-	while ((p = strtok(opt_arg, ",")) != NULL) {
+	opt_tmp = strdup(opt_arg);
+	if (!opt_tmp)
+		return -ENOMEM;
+
+	while ((p = strtok(opt_tmp, ",")) != NULL) {
 		flag = 0;
 		ptr_argv = all_sort_items;
 
@@ -1254,10 +1260,10 @@ int btrfs_qgroup_parse_sort_string(char *opt_arg,
 			ptr_argv++;
 		}
 
-		if (flag == 0)
-			return -1;
-
-		else {
+		if (flag == 0) {
+			ret = -1;
+			goto out;
+		} else {
 			if (*p == '+') {
 				order = 0;
 				p++;
@@ -1268,14 +1274,19 @@ int btrfs_qgroup_parse_sort_string(char *opt_arg,
 				order = 0;
 
 			what_to_sort = btrfs_qgroup_get_sort_item(p);
-			if (what_to_sort < 0)
-				return -1;
+			if (what_to_sort < 0) {
+				ret = -1;
+				goto out;
+			}
 			btrfs_qgroup_setup_comparer(comps, what_to_sort, order);
 		}
-		opt_arg = NULL;
+		free(opt_tmp);
+		opt_tmp = NULL;
 	}
 
-	return 0;
+out:
+	free(opt_tmp);
+	return ret;
 }
 
 int qgroup_inherit_size(struct btrfs_qgroup_inherit *p)

commit 55d2e53207236ac458c0184ffec935f4f9995769

btrfs-progs: qgroup: fix qgroup show sort by multi items
We should not free the string until we don't call strtok any longer.

If the string is freed in advance, in fact, the second and subsequent
sort items will be ignored.

Fixes: 9fcdf8f8945b ("btrfs-progs: don't write to optarg in btrfs_qgroup_parse_sort_string")
Signed-off-by: Lu Fengqi <lufq.fnst@cn.fujitsu.com>
Signed-off-by: David Sterba <dsterba@suse.com>

qgroup.c
@@ -1222,7 +1222,8 @@ int btrfs_qgroup_parse_sort_string(const char *opt_arg,
 	if (!opt_tmp)
 		return -ENOMEM;
 
-	while ((p = strtok(opt_tmp, ",")) != NULL) {
+	p = strtok(opt_tmp, ",");
+	while (p) {
 		flag = 0;
 		ptr_argv = all_sort_items;
 
@@ -1262,8 +1263,7 @@ int btrfs_qgroup_parse_sort_string(const char *opt_arg,
 			}
 			btrfs_qgroup_setup_comparer(comps, what_to_sort, order);
 		}
-		free(opt_tmp);
-		opt_tmp = NULL;
+		p = strtok(NULL, ",");
 	}
 
 out: