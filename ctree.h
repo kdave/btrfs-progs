1f656a2509658dbf703546affa90f274d142b772
Josh Soref <jsoref@users.noreply.github.com>
2018-11-25 22:29:17 -0500

    spelling: endianness

--- ctree.h
+++ ctree.h
@@ -2404,7 +2404,7 @@ static inline struct btrfs_disk_balance_args* btrfs_balance_item_sys(
 
 /*
  * btrfs_dev_stats_item helper, returns pointer to the raw array, do the
- * endiannes conversion, @dsi is offset to eb data
+ * endianness conversion, @dsi is offset to eb data
  */
 static inline __le64* btrfs_dev_stats_values(struct extent_buffer *eb,
 		struct btrfs_dev_stats_item *dsi)
