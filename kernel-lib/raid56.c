b9f109b65d1d1edf054160e051a1e2755c96064a
Josh Soref <jsoref@users.noreply.github.com>
2018-11-25 23:17:06 -0500

    spelling: regenerate

--- kernel-lib/raid56.c
+++ kernel-lib/raid56.c
@@ -330,7 +330,7 @@ int raid56_recov(int nr_devs, size_t stripe_len, u64 profile, int dest1,
 			return 0;
 		}
 
-		/* Regerneate data from P */
+		/* Regenerate data from P */
 		return raid5_gen_result(nr_devs - 1, stripe_len, dest1, data);
 	}
 
