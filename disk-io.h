e4ca845778f72d379f9d79e2471ee30d76b802c7
Qu Wenruo <wqu@suse.com>
2019-01-03 15:32:18 +0800

    btrfs-progs: mkfs: Create data reloc tree from scratch
    For data reloc tree creation, we copy its contents from the fs tree just
    for its INODE_ITEM, INODE_REF and dirid.  This hides the detail and is
    not obvious for why we're copying from fs root.
    
    This patch will create data reloc tree from scratch:
    - Create root, including root item and new tree root
    - Change dirid to BTRFS_FIRST_FREE_OBJECTID
    - Insert root INODE_ITEM and INODE_REF
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- disk-io.h
+++ disk-io.h
@@ -197,6 +197,7 @@ int write_tree_block(struct btrfs_trans_handle *trans,
 		     struct btrfs_fs_info *fs_info,
 		     struct extent_buffer *eb);
 int write_and_map_eb(struct btrfs_fs_info *fs_info, struct extent_buffer *eb);
+int btrfs_fs_roots_compare_roots(struct rb_node *node1, struct rb_node *node2);
 struct btrfs_root *btrfs_create_tree(struct btrfs_trans_handle *trans,
 				     struct btrfs_fs_info *fs_info,
 				     u64 objectid);
