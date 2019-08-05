fcebb87f524956d5d1c082bd4496b574b17a1d5d
Nikolay Borisov <nborisov@suse.com>
2019-08-05 14:45:22 +0300

    btrfs-progs: tests: Check for metadata_uuid feature in misc-tests/034-metadata-uuid
    Instead of checking the kernel version, explicitly check for the
    presence of metadata_uuid file in sysfs. This allows the test to be run
    on older kernels that might have this feature backported.
    
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/misc-tests/034-metadata-uuid/test.sh
+++ tests/misc-tests/034-metadata-uuid/test.sh
@@ -10,8 +10,8 @@ check_prereq btrfs-image
 setup_root_helper
 prepare_test_dev
 
-if ! check_min_kernel_version 5.0; then
-	_not_run "kernel too old, METADATA_UUID support needed"
+if [ ! -f /sys/fs/btrfs/features/metadata_uuid ] ; then
+	_not_run "METADATA_UUID feature not supported"
 fi
 
 read_fsid() {
