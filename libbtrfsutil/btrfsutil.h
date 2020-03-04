16fcd500558118c8432bda0f2192d69b99a0af27
David Sterba <dsterba@suse.com>
2020-03-04 01:34:29 +0100

    libbtrfsutil: bump version to 1.2
    New function:
    
     * btrfs_util_delete_subvolume_by_id_fd
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- libbtrfsutil/btrfsutil.h
+++ libbtrfsutil/btrfsutil.h
@@ -26,8 +26,8 @@
 #include <sys/time.h>
 
 #define BTRFS_UTIL_VERSION_MAJOR 1
-#define BTRFS_UTIL_VERSION_MINOR 1
-#define BTRFS_UTIL_VERSION_PATCH 1
+#define BTRFS_UTIL_VERSION_MINOR 2
+#define BTRFS_UTIL_VERSION_PATCH 0
 
 #ifdef __cplusplus
 extern "C" {
