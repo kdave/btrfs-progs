38f220d6eea51344603e5a213c21500ec671b8b8
Qu Wenruo <wqu@suse.com>
2020-07-29 16:40:38 +0800

    btrfs-progs: convert: report available space before conversion happens
    Now if an ENOSPC error happened, the free space report would help user
    to determine if it's a real ENOSPC or a bug in convert.
    
    The reported free space is the calculated free space, which doesn't
    include super block space, nor merged data chunks.
    
    The free space is always smaller than the reported available space of
    the original fs, as we need extra padding space for used space to avoid
    too fragmented data chunks.
    
    The output would be:
    
    $ ./btrfs-convert /dev/sda
    create btrfs filesystem:
            blocksize: 4096
            nodesize:  16384
            features:  extref, skinny-metadata (default)
            checksum:  crc32c
    free space report:
            total:     10737418240
            free:      0 (0.00%)
    ERROR: unable to create initial ctree: No space left on device
    WARNING: an error occurred during conversion, the original filesystem is not modified
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    [ put total, free to separate lines ]
    Signed-off-by: David Sterba <dsterba@suse.com>

--- convert/common.h
+++ convert/common.h
@@ -35,6 +35,7 @@ struct btrfs_convert_context {
 	u64 inodes_count;
 	u64 free_inodes_count;
 	u64 total_bytes;
+	u64 free_bytes_initial;
 	char *volume_name;
 	const struct btrfs_convert_operations *convert_ops;
 
@@ -47,6 +48,13 @@ struct btrfs_convert_context {
 	/* Free space which is not covered by data_chunks */
 	struct cache_tree free_space;
 
+	/*
+	 * Free space reserved for ENOSPC report, it's just a copy free_space.
+	 * But after initial calculation, free_space_initial is no longer
+	 * updated, so we have a good idea on how much free space we really
+	 * have for btrfs.
+	 */
+	struct cache_tree free_space_initial;
 	void *fs_data;
 };
 
