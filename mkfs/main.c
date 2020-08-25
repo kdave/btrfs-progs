73487311e9f3ba6de48754809376beefa7b34545
Josef Bacik <josef@toxicpanda.com>
2020-08-25 12:51:02 -0400

    btrfs-progs: mkfs: add a warning label for RAID5/6
    We all know there's some dark and scary corners with RAID5/6, but users
    may not know.  Add a warning message in mkfs so anybody trying to use
    this will know things can go very wrong.
    
    Issue: #265
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    [ reword message ]
    Signed-off-by: David Sterba <dsterba@suse.com>

--- mkfs/main.c
+++ mkfs/main.c
@@ -1201,6 +1201,8 @@ int BOX_MAIN(mkfs)(int argc, char **argv)
 	if ((data_profile | metadata_profile) &
 	    (BTRFS_BLOCK_GROUP_RAID5 | BTRFS_BLOCK_GROUP_RAID6)) {
 		features |= BTRFS_FEATURE_INCOMPAT_RAID56;
+		warning("RAID5/6 support has known problems is strongly discouraged\n"
+			"\t to be used besides testing or evaluation.\n");
 	}
 
 	if ((data_profile | metadata_profile) &
