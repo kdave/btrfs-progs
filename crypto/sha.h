fb98c1e9fc67a2aca7b649e7d5a111e85323662f
David Sterba <dsterba@suse.com>
2021-02-18 17:23:22 +0100

    btrfs-progs: crypto: remove unused sha256 definitions
    Remove:
    
    - sha1, sha384, sha512 related structures and helpers
    - HKDF definitions
    - USHA definitions
    
    Keep HMAC.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- crypto/sha.h
+++ crypto/sha.h
@@ -102,44 +102,24 @@ enum {
  *  hashing operations
  */
 enum {
-    SHA1_Message_Block_Size = 64, SHA224_Message_Block_Size = 64,
-    SHA256_Message_Block_Size = 64, SHA384_Message_Block_Size = 128,
-    SHA512_Message_Block_Size = 128,
-    USHA_Max_Message_Block_Size = SHA512_Message_Block_Size,
+    SHA224_Message_Block_Size = 64,
+    SHA256_Message_Block_Size = 64,
+    USHA_Max_Message_Block_Size = SHA256_Message_Block_Size,
 
-    SHA1HashSize = 20, SHA224HashSize = 28, SHA256HashSize = 32,
-    SHA384HashSize = 48, SHA512HashSize = 64,
-    USHAMaxHashSize = SHA512HashSize,
+    SHA224HashSize = 28, SHA256HashSize = 32,
+    USHAMaxHashSize = SHA256HashSize,
 
-    SHA1HashSizeBits = 160, SHA224HashSizeBits = 224,
-    SHA256HashSizeBits = 256, SHA384HashSizeBits = 384,
-    SHA512HashSizeBits = 512, USHAMaxHashSizeBits = SHA512HashSizeBits
+    SHA224HashSizeBits = 224,
+    SHA256HashSizeBits = 256,
+    USHAMaxHashSizeBits = SHA256HashSizeBits
 };
 
 /*
  *  These constants are used in the USHA (Unified SHA) functions.
  */
 typedef enum SHAversion {
-    SHA1, SHA224, SHA256, SHA384, SHA512
+    SHA224, SHA256,
 } SHAversion;
-
-/*
- *  This structure will hold context information for the SHA-1
- *  hashing operation.
- */
-typedef struct SHA1Context {
-    uint32_t Intermediate_Hash[SHA1HashSize/4]; /* Message Digest */
-
-    uint32_t Length_High;               /* Message length in bits */
-    uint32_t Length_Low;                /* Message length in bits */
-
-    int_least16_t Message_Block_Index;  /* Message_Block array index */
-                                        /* 512-bit message blocks */
-    uint8_t Message_Block[SHA1_Message_Block_Size];
-
-    int Computed;                   /* Is the hash computed? */
-    int Corrupted;                  /* Cumulative corruption code */
-} SHA1Context;
 
 /*
  *  This structure will hold context information for the SHA-256
@@ -159,26 +139,6 @@ typedef struct SHA256Context {
     int Corrupted;                  /* Cumulative corruption code */
 } SHA256Context;
 
-/*
- *  This structure will hold context information for the SHA-512
- *  hashing operation.
- */
-typedef struct SHA512Context {
-#ifdef USE_32BIT_ONLY
-    uint32_t Intermediate_Hash[SHA512HashSize/4]; /* Message Digest  */
-    uint32_t Length[4];                 /* Message length in bits */
-#else /* !USE_32BIT_ONLY */
-    uint64_t Intermediate_Hash[SHA512HashSize/8]; /* Message Digest */
-    uint64_t Length_High, Length_Low;   /* Message length in bits */
-#endif /* USE_32BIT_ONLY */
-
-    int_least16_t Message_Block_Index;  /* Message_Block array index */
-                                        /* 1024-bit message blocks */
-    uint8_t Message_Block[SHA512_Message_Block_Size];
-
-    int Computed;                   /* Is the hash computed?*/
-    int Corrupted;                  /* Cumulative corruption code */
-} SHA512Context;
 
 /*
  *  This structure will hold context information for the SHA-224
@@ -187,21 +147,13 @@ typedef struct SHA512Context {
 typedef struct SHA256Context SHA224Context;
 
 /*
- *  This structure will hold context information for the SHA-384
- *  hashing operation.  It uses the SHA-512 structure for computation.
- */
-typedef struct SHA512Context SHA384Context;
-
-/*
  *  This structure holds context information for all SHA
  *  hashing operations.
  */
 typedef struct USHAContext {
     int whichSha;               /* which SHA is being used */
     union {
-      SHA1Context sha1Context;
       SHA224Context sha224Context; SHA256Context sha256Context;
-      SHA384Context sha384Context; SHA512Context sha512Context;
     } ctx;
 } USHAContext;
 
@@ -222,31 +174,8 @@ typedef struct HMACContext {
 } HMACContext;
 
 /*
- *  This structure will hold context information for the HKDF
- *  extract-and-expand Key Derivation Functions.
- */
-typedef struct HKDFContext {
-    int whichSha;               /* which SHA is being used */
-    HMACContext hmacContext;
-    int hashSize;               /* hash size of SHA being used */
-    unsigned char prk[USHAMaxHashSize];
-                        /* pseudo-random key - output of hkdfInput */
-    int Computed;               /* Is the key material computed? */
-    int Corrupted;              /* Cumulative corruption code */
-} HKDFContext;
-
-/*
  *  Function Prototypes
  */
-
-/* SHA-1 */
-extern int SHA1Reset(SHA1Context *);
-extern int SHA1Input(SHA1Context *, const uint8_t *bytes,
-                     unsigned int bytecount);
-extern int SHA1FinalBits(SHA1Context *, uint8_t bits,
-                         unsigned int bit_count);
-extern int SHA1Result(SHA1Context *,
-                      uint8_t Message_Digest[SHA1HashSize]);
 
 /* SHA-224 */
 extern int SHA224Reset(SHA224Context *);
@@ -266,49 +195,6 @@ extern int SHA256FinalBits(SHA256Context *, uint8_t bits,
 extern int SHA256Result(SHA256Context *,
                         uint8_t Message_Digest[SHA256HashSize]);
 
-/* SHA-384 */
-extern int SHA384Reset(SHA384Context *);
-extern int SHA384Input(SHA384Context *, const uint8_t *bytes,
-                       unsigned int bytecount);
-extern int SHA384FinalBits(SHA384Context *, uint8_t bits,
-                           unsigned int bit_count);
-extern int SHA384Result(SHA384Context *,
-                        uint8_t Message_Digest[SHA384HashSize]);
-
-/* SHA-512 */
-extern int SHA512Reset(SHA512Context *);
-extern int SHA512Input(SHA512Context *, const uint8_t *bytes,
-                       unsigned int bytecount);
-extern int SHA512FinalBits(SHA512Context *, uint8_t bits,
-                           unsigned int bit_count);
-extern int SHA512Result(SHA512Context *,
-                        uint8_t Message_Digest[SHA512HashSize]);
-
-/* Unified SHA functions, chosen by whichSha */
-extern int USHAReset(USHAContext *context, SHAversion whichSha);
-extern int USHAInput(USHAContext *context,
-                     const uint8_t *bytes, unsigned int bytecount);
-extern int USHAFinalBits(USHAContext *context,
-                         uint8_t bits, unsigned int bit_count);
-extern int USHAResult(USHAContext *context,
-                      uint8_t Message_Digest[USHAMaxHashSize]);
-extern int USHABlockSize(enum SHAversion whichSha);
-extern int USHAHashSize(enum SHAversion whichSha);
-extern int USHAHashSizeBits(enum SHAversion whichSha);
-extern const char *USHAHashName(enum SHAversion whichSha);
-
-/*
- * HMAC Keyed-Hashing for Message Authentication, RFC 2104,
- * for all SHAs.
- * This interface allows a fixed-length text input to be used.
- */
-extern int hmac(SHAversion whichSha, /* which SHA algorithm to use */
-    const unsigned char *text,     /* pointer to data stream */
-    int text_len,                  /* length of data stream */
-    const unsigned char *key,      /* pointer to authentication key */
-    int key_len,                   /* length of authentication key */
-    uint8_t digest[USHAMaxHashSize]); /* caller digest to fill in */
-
 /*
  * HMAC Keyed-Hashing for Message Authentication, RFC 2104,
  * for all SHAs.
@@ -323,34 +209,4 @@ extern int hmacFinalBits(HMACContext *context, uint8_t bits,
 extern int hmacResult(HMACContext *context,
                       uint8_t digest[USHAMaxHashSize]);
 
-/*
- * HKDF HMAC-based Extract-and-Expand Key Derivation Function,
- * RFC 5869, for all SHAs.
- */
-extern int hkdf(SHAversion whichSha, const unsigned char *salt,
-                int salt_len, const unsigned char *ikm, int ikm_len,
-                const unsigned char *info, int info_len,
-                uint8_t okm[ ], int okm_len);
-extern int hkdfExtract(SHAversion whichSha, const unsigned char *salt,
-                       int salt_len, const unsigned char *ikm,
-                       int ikm_len, uint8_t prk[USHAMaxHashSize]);
-extern int hkdfExpand(SHAversion whichSha, const uint8_t prk[ ],
-                      int prk_len, const unsigned char *info,
-                      int info_len, uint8_t okm[ ], int okm_len);
-
-/*
- * HKDF HMAC-based Extract-and-Expand Key Derivation Function,
- * RFC 5869, for all SHAs.
- * This interface allows any length of text input to be used.
- */
-extern int hkdfReset(HKDFContext *context, enum SHAversion whichSha,
-                     const unsigned char *salt, int salt_len);
-extern int hkdfInput(HKDFContext *context, const unsigned char *ikm,
-                     int ikm_len);
-extern int hkdfFinalBits(HKDFContext *context, uint8_t ikm_bits,
-                         unsigned int ikm_bit_count);
-extern int hkdfResult(HKDFContext *context,
-                      uint8_t prk[USHAMaxHashSize],
-                      const unsigned char *info, int info_len,
-                      uint8_t okm[USHAMaxHashSize], int okm_len);
 #endif /* _SHA_H_ */
