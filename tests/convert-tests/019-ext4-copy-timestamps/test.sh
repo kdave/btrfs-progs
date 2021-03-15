0c6e50dddd6be96a775e6015c7e331456a0cda14
David Sterba <dsterba@suse.com>
2021-03-15 16:29:37 +0100

    btrfs-progs: tests: verify that ext4 supports extra_isize
    To properly check the 64bit timestamp conversion, the filesystem must
    support it. Check that a freshly created filesystem contains the
    feature.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/convert-tests/019-ext4-copy-timestamps/test.sh
+++ tests/convert-tests/019-ext4-copy-timestamps/test.sh
@@ -10,6 +10,12 @@ check_prereq btrfs-convert
 check_global_prereq mke2fs
 
 convert_test_prep_fs ext4 mke2fs -t ext4 -b 4096
+
+features=$(run_check_stdout dumpe2fs "$TEST_DEV" | grep "Filesystem features")
+if ! echo "$features" | grep -q "extra_isize"; then
+	_not_run "extended inode size not supported, no 64bit timestamp check possible"
+fi
+
 run_check $SUDO_HELPER touch "$TEST_MNT/file"
 # Read time values
 run_check stat "$TEST_MNT/file"
