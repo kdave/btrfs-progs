kdave/btrfs-progs

commit 1ed5db8db445073eb0d8b807901b64edaac1d8c4

btrfs-progs: balance convert: add a warning and countdown for RAID56 conversion
Similar to the mkfs warning, add a warning to btrfs balance convert
options, with a countdown to allow the user to have time to cancel the
operation.

Issue: #265
Signed-off-by: Josef Bacik <josef@toxicpanda.com>
Signed-off-by: David Sterba <dsterba@suse.com>

cmds/balance.c
@@ -526,6 +526,7 @@ static int cmd_balance_start(const struct cmd_struct *cmd,
 	int background = 0;
 	bool enqueue = false;
 	unsigned start_flags = 0;
+	bool warned = false;
 	int i;
 
 	memset(&args, 0, sizeof(args));
@@ -632,11 +633,37 @@ static int cmd_balance_start(const struct cmd_struct *cmd,
 
 	/* soft makes sense only when convert for corresponding type is set */
 	for (i = 0; ptrs[i]; i++) {
+		int delay = 10;
+
 		if ((ptrs[i]->flags & BTRFS_BALANCE_ARGS_SOFT) &&
 		    !(ptrs[i]->flags & BTRFS_BALANCE_ARGS_CONVERT)) {
 			error("'soft' option can be used only when converting profiles");
 			return 1;
 		}
+
+		if (!(ptrs[i]->flags & BTRFS_BALANCE_ARGS_CONVERT))
+			continue;
+
+		if (!(ptrs[i]->flags & (BTRFS_BLOCK_GROUP_RAID6 |
+					BTRFS_BLOCK_GROUP_RAID5)))
+			continue;
+
+		if (warned)
+			continue;
+
+		warned = true;
+		printf("WARNING:\n\n");
+		printf("\tRAID5/6 support has known problems and is strongly discouraged\n");
+		printf("\tto be used besides testing or evaluation. It is recommended that\n");
+		printf("\tyou use one of the other RAID profiles.\n");
+		printf("\tThe operation will continue in %d seconds.\n", delay);
+		printf("\tUse Ctrl-C to stop.\n");
+		while (delay) {
+			printf("%2d", delay--);
+			fflush(stdout);
+			sleep(1);
+		}
+		printf("\nStarting conversion to RAID5/6.\n");
 	}
 
 	if (!(start_flags & BALANCE_START_FILTERS) && !(start_flags & BALANCE_START_NOWARN)) {

commit 3a22f0b25bbddf3b2eb1fe2d7bcba8554e451c82

btrfs-progs: balance: fix raid56 warning for other profiles
The warning is printed for profiles where it's not intended (like raid0
or raid1c4). Check the correct variable for the target profiles.

Issue: #355
Fixes: 1ed5db8db445 ("btrfs-progs: balance convert: add a warning and countdown for RAID56 conversion")
Signed-off-by: David Sterba <dsterba@suse.com>

cmds/balance.c
@@ -645,8 +645,8 @@ static int cmd_balance_start(const struct cmd_struct *cmd,
 		if (!(ptrs[i]->flags & BTRFS_BALANCE_ARGS_CONVERT))
 			continue;
 
-		if (!(ptrs[i]->flags & (BTRFS_BLOCK_GROUP_RAID6 |
-					BTRFS_BLOCK_GROUP_RAID5)))
+		if (!(ptrs[i]->target & (BTRFS_BLOCK_GROUP_RAID6 |
+					   BTRFS_BLOCK_GROUP_RAID5)))
 			continue;
 
 		if (raid56_warned)

tests/misc-tests/047-raid5-convert/test.sh
@@ -1,0 +1,35 @@
+#!/bin/bash
+# Verify that raid56 warning is printed before balance conversion when the
+# target profile is raid5 or raid6, but not other profiles
+
+source "$TEST_TOP/common"
+
+check_prereq mkfs.btrfs
+check_prereq btrfs
+
+setup_root_helper
+setup_loopdevs 6
+prepare_loopdevs
+
+TEST_DEV=${loopdevs[1]}
+
+run_check $SUDO_HELPER "$TOP/mkfs.btrfs" -f -d single -m single "${loopdevs[@]}"
+run_check_mount_test_dev
+stdout=$(run_check_stdout $SUDO_HELPER "$TOP/btrfs" balance start -mconvert=raid5 "$TEST_MNT")
+if ! echo "$stdout" | grep -q WARNING; then
+	_fail "no warning for balance conversion"
+fi
+run_check $SUDO_HELPER "$TOP/btrfs" balance start -f -mconvert=raid6 "$TEST_MNT"
+run_mayfail $SUDO_HELPER "$TOP/btrfs" balance start -mconvert=raid0 "$TEST_MNT"
+run_check $SUDO_HELPER "$TOP/btrfs" balance start -f -mconvert=raid0 "$TEST_MNT"
+if run_check_stdout $SUDO_HELPER "$TOP/btrfs" balance start -mconvert=raid1c3 "$TEST_MNT" |
+		grep -q WARNING; then
+	_fail "warning found for balance conversion"
+fi
+if run_check_stdout $SUDO_HELPER "$TOP/btrfs" balance start -mconvert=raid1c4 "$TEST_MNT" |
+		grep -q WARNING; then
+	_fail "warning found for balance conversion"
+fi
+run_check_umount_test_dev
+
+cleanup_loopdevs