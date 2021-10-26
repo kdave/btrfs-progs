fb9c88fa93fe3c190a3a0b5e4c5edea3233bfcdb
David Sterba <dsterba@suse.com>
2021-10-27 00:45:24 +0200

    btrfs-progs: docs: convert btrfs-image to rst
    Signed-off-by: David Sterba <dsterba@suse.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -53,4 +53,5 @@ man_pages = [
     ('btrfs-qgroup', 'btrfs-qgroup', 'control the quota group of a btrfs filesystem', '', 8),
     ('btrfs-property', 'btrfs-property', 'get/set/list properties for given filesystem object', '', 8),
     ('btrfs-inspect-internal', 'btrfs-inspect-internal', 'query various internal information', '', 8),
+    ('btrfs-image', 'btrfs-image', 'create/restore an image of the filesystem', '', 8),
 ]
