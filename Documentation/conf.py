86d39041668853cb959a6a73722ab334f3914c77
David Sterba <dsterba@suse.com>
2021-10-26 22:48:27 +0200

    btrfs-progs: docs: add sphinx build support
    Add basic config and build files for sphinx. The long term plan is to
    convert all asciidoc sources to ReST and host majority of wiki contents,
    and maybe more.
    
    Build:
    
      $ cd Documentation/
      $ make -f Makefile.sphinx html
    
    The result is in Documentation/_build/html.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

---
+++ Documentation/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+project = 'BTRFS'
+# TODO: date
+copyright = '2021'
+
+# TODO: copy from ../VERSION
+release = '5.14.2'
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build']
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ Documentation/index.rst
@@ -0,0 +1,20 @@
+.. BTRFS documentation master file, created by
+   sphinx-quickstart on Tue Oct 26 22:26:45 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to BTRFS documentation!
+===============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
