e8f7f034afb0a1b44293fe00731495829d748ad4
Josef Bacik <josef@toxicpanda.com>
2022-03-07 17:11:04 -0500

    btrfs-progs: check: don't do the root item check for extent tree v2
    With the current set of changes we could probably do this check, but it
    would involve changing the code quite a bit, and in the future we're not
    going to track the metadata in the extent tree at all.  Since this check
    was for a very old kernel just skip it for extent tree v2.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- check/main.c
+++ check/main.c
@@ -9860,6 +9860,9 @@ static int repair_root_items(void)
 	int bad_roots = 0;
 	int need_trans = 0;
 
+	if (btrfs_fs_incompat(gfs_info, EXTENT_TREE_V2))
+		return 0;
+
 	btrfs_init_path(&path);
 
 	ret = build_roots_info_cache();
