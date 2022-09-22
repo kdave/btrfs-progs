9cb5b0d058071bc334ed54246484f6079df216e8
David Sterba <dsterba@suse.com>
2022-09-22 15:04:00 +0200

    btrfs-progs: tests: in mkfs/002 use grep -E instead of egrep
    The egrep command is deprecated (per manual page of grep) for a long
    time and will probably be removed, the replacement is 'grep -E'.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/mkfs-tests/002-no-force-mixed-on-small-volume/test.sh
+++ tests/mkfs-tests/002-no-force-mixed-on-small-volume/test.sh
@@ -8,5 +8,5 @@ check_prereq mkfs.btrfs
 
 setup_root_helper
 
-mixed=$(run_check_stdout "$TOP/mkfs.btrfs" -b 512M -n 64k -f "$TEST_DEV" | egrep 'Data|Metadata')
+mixed=$(run_check_stdout "$TOP/mkfs.btrfs" -b 512M -n 64k -f "$TEST_DEV" | grep -E 'Data|Metadata')
 echo "$mixed" | grep -q -v 'Data+Metadata:' || _fail "unexpected: created a mixed-bg filesystem"
