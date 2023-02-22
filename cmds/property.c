8c0d870ac7a98cb122cd6312a4e3f0e54a755c5d
David Sterba <dsterba@suse.com>
2023-02-22 01:37:18 +0100

    btrfs-progs: property: convert help text to option formatter
    Signed-off-by: David Sterba <dsterba@suse.com>

--- cmds/property.c
+++ cmds/property.c
@@ -582,8 +582,7 @@ static const char * const cmd_property_get_usage[] = {
 	"Possible values for type are: inode, subvol, filesystem, device.",
 	"They can be abbreviated to the first letter, i/s/f/d",
 	"",
-	"-t <TYPE>       list properties for the given object type (inode, subvol,",
-	"                filesystem, device)",
+	OPTLINE("-t <TYPE>", "list properties for the given object type (inode, subvol, filesystem, device)"),
 	NULL
 };
 
@@ -614,9 +613,8 @@ static const char * const cmd_property_set_usage[] = {
 	"Set a property on a btrfs object where object is a path to file or",
 	"directory and can also represent the filesystem or device based on the type",
 	"",
-	"-t <TYPE>       list properties for the given object type (inode, subvol,",
-	"                filesystem, device)",
-	"-f              force the change, could potentially break something\n",
+	OPTLINE("-t <TYPE>", "list properties for the given object type (inode, subvol, filesystem, device)"),
+	OPTLINE("-f", "force the change, could potentially break something"),
 	NULL
 };
 
@@ -646,8 +644,7 @@ static const char * const cmd_property_list_usage[] = {
 	"See the help of 'btrfs property get' for a description of",
 	"objects and object types.",
 	"",
-	"-t <TYPE>       list properties for the given object type (inode, subvol,",
-	"                filesystem, device)",
+	OPTLINE("-t <TYPE>", "list properties for the given object type (inode, subvol, filesystem, device)"),
 	NULL
 };
 
