kdave/btrfs-progs

commit 6f7151f4992bc2a22a4fb84e7536a8b3c709a771

btrfs-progs: balance: fix some cases wrongly parsed as old syntax
Some cases of 'btrfs balance' are wrongly parsed as old syntax.
  $ btrfs balance status
  ERROR: cannot access 'status': No such file or directory

Currently, only 'start' is successfully excluded in the check of old
syntax.  Fix it by adding others in the check of old syntax.

Signed-off-by: Wang Yugui <wangyugui@e16-tech.com>
Signed-off-by: David Sterba <dsterba@suse.com>

cmds/balance.c
@@ -865,8 +865,24 @@ static const struct cmd_group balance_cmd_group = {
 
 static int cmd_balance(const struct cmd_struct *cmd, int argc, char **argv)
 {
-	if (argc == 2 && strcmp("start", argv[1]) != 0) {
-		/* old 'btrfs filesystem balance <path>' syntax */
+	bool old_syntax = true;
+
+	/*
+	 * Exclude all valid subcommands from being potentially confused as path
+	 * for the obsolete syntax: btrfs balance <path>
+	 */
+	if (argc >= 2) {
+		for (int i = 0; balance_cmd_group.commands[i] != NULL; i++) {
+			if (strcmp(argv[1], balance_cmd_group.commands[i]->token) == 0) {
+				old_syntax = false;
+				break;
+			}
+		}
+	} else {
+		old_syntax = false;
+	}
+
+	if (old_syntax) {
 		struct btrfs_ioctl_balance_args args;
 
 		memset(&args, 0, sizeof(args));

commit 9cb34031d729bf9f658b57c62b4da5ba23677564

btrfs-progs: balance: fix condition for recognizing old syntax
The commit 6f7151f4992b extended the set of recognized valid subcommands
for the old path syntax but wrongly checks for more than 2 parameters.
That way a shortened and valid new syntax is not recognized (here 'can'
is short for 'cancel' and the short form is not in the list):

  btrfs-progs-6.1.3

  btrfs bal can /
  ERROR: balance cancel on '/' failed: Not in progress

  btrfs-progs-6.2.2

  btrfs bal can /
  WARNING: deprecated syntax, please use 'btrfs balance start'
  ERROR: cannot access 'can': No such file or directory

Issue: #612
Fixes: 6f7151f4992b ("btrfs-progs: balance: fix some cases wrongly parsed as old syntax")
Signed-off-by: David Sterba <dsterba@suse.com>

cmds/balance.c
@@ -871,7 +871,7 @@ static int cmd_balance(const struct cmd_struct *cmd, int argc, char **argv)
 	 * Exclude all valid subcommands from being potentially confused as path
 	 * for the obsolete syntax: btrfs balance <path>
 	 */
-	if (argc >= 2) {
+	if (argc == 2) {
 		for (int i = 0; balance_cmd_group.commands[i] != NULL; i++) {
 			if (strcmp(argv[1], balance_cmd_group.commands[i]->token) == 0) {
 				old_syntax = false;