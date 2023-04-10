30fde12728bf1cdc2834c6eebe24ec7550b9a9fe
Josef Bacik <josef@toxicpanda.com>
2023-04-10 14:11:31 -0400

    btrfs-progs: tests: fix fsck-tests/056 to run without root
    We need to make sure the root helper is setup before calling the loop
    helpers, and additionally we need to use $SUDO_HELPER when we run the
    final btrfs check.  With this patch we can now run this test as a normal
    user.
    
    Reviewed-by: Neal Gompa <neal@gompa.dev>
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/fsck-tests/056-raid56-false-alerts/test.sh
+++ tests/fsck-tests/056-raid56-false-alerts/test.sh
@@ -10,12 +10,12 @@ check_prereq btrfs
 check_prereq mkfs.btrfs
 check_global_prereq losetup
 
+setup_root_helper
+
 setup_loopdevs 3
 prepare_loopdevs
 dev1=${loopdevs[1]}
 TEST_DEV=$dev1
-
-setup_root_helper
 
 run_check $SUDO_HELPER "$TOP/mkfs.btrfs" -f -m raid1 -d raid5 "${loopdevs[@]}"
 run_check_mount_test_dev
@@ -26,6 +26,6 @@ run_check $SUDO_HELPER dd if=/dev/urandom of="$TEST_MNT/file" bs=16K count=1 \
 run_check_umount_test_dev
 
 # Check data csum should not report false alerts
-run_check "$TOP/btrfs" check --check-data-csum "$dev1"
+run_check $SUDO_HELPER "$TOP/btrfs" check --check-data-csum "$dev1"
 
 cleanup_loopdevs
