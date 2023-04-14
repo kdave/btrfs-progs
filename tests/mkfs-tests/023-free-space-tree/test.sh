5a0398bac5a3264de46abd7021ddc5bd9b24043f
David Sterba <dsterba@suse.com>
2023-04-14 17:48:41 +0200

    btrfs-progs: tests: mkfs/023, use option -O for free-space-tree
    The option -O now allows to set all features at mkfs time, use it.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/mkfs-tests/023-free-space-tree/test.sh
+++ tests/mkfs-tests/023-free-space-tree/test.sh
@@ -9,7 +9,7 @@ check_prereq btrfs
 setup_root_helper
 prepare_test_dev
 
-run_check_mkfs_test_dev -R free-space-tree
+run_check_mkfs_test_dev -O free-space-tree
 
 run_check_stdout $SUDO_HELPER "$TOP/btrfs" inspect-internal dump-super "$TEST_DEV" | \
 	grep -q FREE_SPACE_TREE || _fail "free space tree not created"
