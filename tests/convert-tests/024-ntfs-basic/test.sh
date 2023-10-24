f195461b0e75907a199c05ba4a16b822d5bd05ec
David Sterba <dsterba@suse.com>
2023-10-24 13:52:36 +0200

    btrfs-progs: tests: add basic test for ntrfs conversion
    The ntfs2btrfs tool recently found a bug in 'check', add the conversion
    test support to our testsuite. It's optional and depends on kernel
    support of the 'ntfs3' driver and the availability of mkntfs and
    ntfs2btrfs, which might not be available everywhere.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

---
+++ tests/convert-tests/024-ntfs-basic/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Really basic test of ntfs2btrfs conversion
+
+source "$TEST_TOP/common" || exit
+source "$TEST_TOP/common.convert" || exit
+
+if ! check_kernel_support_ntfs >/dev/null; then
+	_not_run "no NTFS support"
+fi
+
+check_prereq btrfs-convert
+check_global_prereq mkfs.ntfs
+check_global_prereq ntfs2btrfs
+
+setup_root_helper
+prepare_test_dev
+
+# Iterate over defaults and options that are not tied to hardware capabilities
+# or number of devices. Test only 4K block size as minimum.
+for feature in '' 'block-group-tree' ; do
+	convert_test ntfs "$feature" "ntfs 4k nodesize" 4096 mkfs.ntfs -s 4096
+done
