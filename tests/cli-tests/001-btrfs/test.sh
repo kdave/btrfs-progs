960e7b3800dcc38de87751d7e715662c6b4ffa72
David Sterba <dsterba@suse.com>
2023-12-01 01:30:45 +0100

    btrfs-progs: tests: test log levels
    The log levels have been added in 6.2, add the basic coverage.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/cli-tests/001-btrfs/test.sh
+++ tests/cli-tests/001-btrfs/test.sh
@@ -16,3 +16,15 @@ run_check "$TOP/btrfs" --help
 run_check "$TOP/btrfs" --help --full
 run_check "$TOP/btrfs" --version
 run_check "$TOP/btrfs" --version --help
+
+# Log levels
+run_check "$TOP/btrfs" --log=default help
+run_check "$TOP/btrfs" --log=info help
+run_check "$TOP/btrfs" -vv help
+run_check "$TOP/btrfs" --log=verbose help
+run_check "$TOP/btrfs" -vvv help
+run_check "$TOP/btrfs" --log=debug help
+run_check "$TOP/btrfs" -vvvv help
+run_check "$TOP/btrfs" --log=quiet help
+run_check "$TOP/btrfs" -q help
+run_mustfail "invalid log level accepted" "$TOP/btrfs" --log=invalid help
