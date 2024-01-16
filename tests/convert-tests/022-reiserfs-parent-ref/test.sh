bbf5a3f72c900efcf3e0d095facc0ca61274da25
David Sterba <dsterba@suse.com>
2024-01-16 20:33:38 +0100

    btrfs-progs: tests: fix 022-reiserfs-parent-ref setup
    The test does not pass in environments where /bin/sh is not bash (like
    github CI). Unify the initial setup like the other tests do: source,
    compatibility, prerequisities, the rest.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/convert-tests/022-reiserfs-parent-ref/test.sh
+++ tests/convert-tests/022-reiserfs-parent-ref/test.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 # Test that only toplevel directory self-reference is created
 
 source "$TEST_TOP/common" || exit
 source "$TEST_TOP/common.convert" || exit
 
-setup_root_helper
-prepare_test_dev
-
 if ! check_kernel_support_reiserfs >/dev/null; then
 	_not_run "no reiserfs support"
 fi
 
-check_global_prereq mkreiserfs
 check_prereq btrfs-convert
+check_global_prereq mkreiserfs
+
+setup_root_helper
+prepare_test_dev
 
 run_check $SUDO_HELPER mkreiserfs -q -f -b 4096 "$TEST_DEV"
 run_check_mount_test_dev -t reiserfs
