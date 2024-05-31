8da61a05855d95b85f6cdc440aee0ffab9187d9b
David Sterba <dsterba@suse.com>
2024-05-31 18:31:44 +0200

    btrfs-progs: tests: update misc/055 to handle longer subvolume cleaning
    After patch "btrfs-progs: qgroup: handle stale qgroup deletion more
    accurately" cleaning stale qgroups may not happen if the subvolume
    cleaning is still in progress.  Update the test so it's able to handle
    that.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/misc-tests/055-qgroup-clear-stale/test.sh
+++ tests/misc-tests/055-qgroup-clear-stale/test.sh
@@ -18,7 +18,14 @@ run_check $SUDO_HELPER "$TOP/btrfs" qgroup show "$TEST_MNT"
 rootid=$(run_check_stdout "$TOP/btrfs" inspect-internal rootid "$TEST_MNT/subv1")
 run_check $SUDO_HELPER "$TOP/btrfs" subvolume delete "$TEST_MNT/subv1"
 run_check $SUDO_HELPER "$TOP/btrfs" qgroup show "$TEST_MNT"
-run_check $SUDO_HELPER "$TOP/btrfs" qgroup clear-stale "$TEST_MNT"
+# Subvolume deletion could be slow and won't let the qgroup to be cleaned,
+# shown as '<under deletion>'.
+if ! run_mayfail $SUDO_HELPER "$TOP/btrfs" qgroup clear-stale "$TEST_MNT"; then
+	run_check $SUDO_HELPER "$TOP/btrfs" subvolume sync "$TEST_MNT" "$rootid"
+	run_check $SUDO_HELPER "$TOP/btrfs" qgroup show "$TEST_MNT"
+	# After cleaning the subvolume it must pass
+	run_check $SUDO_HELPER "$TOP/btrfs" qgroup clear-stale "$TEST_MNT"
+fi
 list=$(run_check_stdout $SUDO_HELPER "$TOP/btrfs" qgroup show "$TEST_MNT")
 if echo "$list" | grep -q "0/$rootid"; then
 	_fail "stale qgroups not deleted"
