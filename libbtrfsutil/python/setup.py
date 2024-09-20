kdave/btrfs-progs

commit 87c3fb557ef2f0f3e623dd976e08a8cb65ea91a6

libbtrfsutil: update python setup.py for distribution
Fixes so 'python3 -m build' works and package can be uploaded to pypi
(https://pypi.org/project/btrfsutil/).

- setup.py is still used for local build (make)
- for pypi it must be done by 'python3 -m build' that is build in a
  temporary directory
- btrfsutilpy.h must be also distributed
- version is set manually (the git VERSION file is not accessible)
- the project page metadata is empty, the README.md should be added

Issue: #310
Signed-off-by: David Sterba <dsterba@suse.com>

libbtrfsutil/python/setup.py
@@ -70,7 +70,12 @@ void add_module_constants(PyObject *m)
 
 class my_build_ext(build_ext):
     def run(self):
-        if out_of_date(['../btrfsutil.h'], 'constants.c'):
+        # Running dist outside of git
+        if not os.path.exists('../btrfsutil.h'):
+            # But no generated constants.c found
+            if not os.path.exists('constants.c'):
+                raise Exception("No generated constants.c found, please fix")
+        elif out_of_date(['../btrfsutil.h'], 'constants.c'):
             try:
                 gen_constants()
             except Exception as e:
@@ -92,6 +97,9 @@ module = Extension(
         'qgroup.c',
         'subvolume.c',
     ],
+    headers=[
+        'btrfsutilpy.h'
+    ],
     include_dirs=['..'],
     library_dirs=['../..'],
     libraries=['btrfsutil'],
@@ -99,7 +107,9 @@ module = Extension(
 
 setup(
     name='btrfsutil',
-    version=get_version(),
+    # FIXME: version file is not present when building outside of git
+    #version=get_version(),
+    version='6.10',
     description='Library for managing Btrfs filesystems',
     url='https://github.com/kdave/btrfs-progs',
     license='LGPLv2+',

commit ef82e575eed908ae0d8371bda7c7f962d38b06a7

btrfs-progs: libbtrfsutil: use package_data member for header files
[BUG]
Currently with python3.12, the python bindding will always result the
following warning:

    [PY]     libbtrfsutil
/usr/lib/python3.12/site-packages/setuptools/_distutils/extension.py:134: UserWarning: Unknown Extension options: 'headers'
  warnings.warn(msg)

[CAUSE]
In the setup.py which specifies the files to be included into the
package, we use setuptools::Extension to specify the file lists and
include paths.

But there is no handling of Extension::headers member, thus resulting
the above warning.

[FIX]
Not an expert in python, but other packages like cffi are all using
`package_data` to include the headers, so just follow them to use
`package_data` member for headers.

Fixes: 87c3fb557ef2 ("libbtrfsutil: update python setup.py for distribution")
Signed-off-by: Qu Wenruo <wqu@suse.com>

libbtrfsutil/python/setup.py
@@ -97,9 +97,6 @@ module = Extension(
         'qgroup.c',
         'subvolume.c',
     ],
-    headers=[
-        'btrfsutilpy.h'
-    ],
     include_dirs=['..'],
     library_dirs=['../..'],
     libraries=['btrfsutil'],
@@ -113,6 +110,7 @@ setup(
     description='Library for managing Btrfs filesystems',
     long_description=open('../README.md').read(),
     long_description_content_type='text/markdown',
+    package_data = { "btrfstuil" : [ "btrfsutilpy.h" ] },
     url='https://github.com/kdave/btrfs-progs',
     license='LGPLv2+',
     cmdclass={'build_ext': my_build_ext},