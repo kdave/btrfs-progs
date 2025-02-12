ac3ecb11d615263a54ac5a893f83bba87a605514
David Sterba <dsterba@suse.com>
2025-02-12 23:01:19 +0100

    btrfs-progs: tests: rename and update convert/027-large-symbol-link
    Use 'symbolic' as the term.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/convert-tests/027-large-symbolic-link/test.sh
+++ tests/convert-tests/027-large-symbolic-link/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Make sure btrfs-convert can handle a symbol link which is 4095 bytes large
+# Make sure btrfs-convert can handle a symbolic link which is 4095 bytes long
 
 source "$TEST_TOP/common" || exit
 source "$TEST_TOP/common.convert" || exit
@@ -12,16 +12,16 @@ check_global_prereq mkfs.ext4
 link_target=$(printf "%0.sb" {1..4095})
 
 convert_test_prep_fs ext4 mke2fs -t ext4 -b 4096
-run_check $SUDO_HELPER ln -s "$link_target" "$TEST_MNT/symbol_link"
+run_check $SUDO_HELPER ln -s "$link_target" "$TEST_MNT/symbolic_link"
 run_check_umount_test_dev
 
 # For unpatched btrfs-convert, it will always append one byte to the
-# link target, causing above 4095 target to be 4096, exactly one sector,
-# resulting a regular file extent.
+# link target, causing the above 4095 target to be 4096, exactly one sector,
+# resulting in a regular file extent.
 convert_test_do_convert
 
 run_check_mount_test_dev
 # If the unpatched btrfs-convert created a regular extent, and the kernel is
-# newer enough, such readlink will be rejected by kernel.
-run_check $SUDO_HELPER readlink "$TEST_MNT/symbol_link"
+# new enough, readlink will be rejected by kernel.
+run_check $SUDO_HELPER readlink "$TEST_MNT/symbolic_link"
 run_check_umount_test_dev
