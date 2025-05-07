kdave/btrfs-progs

commit 88dc309aca104b027c9eeda70b8050a29df2bbbd

btrfs-progs: check: explicit holes in log tree don't get csummed
Fix a false positive in btrfs check, where we were returning an error
because an explicit hole in the log tree had no associated csum entry.

Signed-off-by: Mark Harmstone <maharmstone@fb.com>

check/main.c
@@ -9694,6 +9694,10 @@ static int check_log_csum(struct btrfs_root *root, u64 addr, u64 length)
 	u64 data_len;
 	int ret;
 
+	/* Explicit holes don't get csummed */
+	if (addr == 0)
+		return 0;
+
 	ret = btrfs_search_slot(NULL, root, &key, &path, 0, 0);
 	if (ret < 0)
 		return ret;

commit da4c6041fafaa99cea62a049e4a37d2c495a5033

btrfs-progs: check: fix false alert on missing csum for hole
If we log a hole, fsync a file, partially write to the front of the hole
and then fsync again the file, we end up with a file extent item in the
log tree that represents a hole and has a disk address of 0 and an offset
that is greater than zero (since we trimmed the front part of the range to
accomodate for a file extent item of the new write).

When this happens 'btrfs check' incorrectly reports that a csum is missing
like this:

  $ btrfs check /dev/sdc
  Opening filesystem to check...
  Checking filesystem on /dev/sdc
  UUID: 46a85f62-4b6e-4aab-bfdc-f08d1bae9e08
  [1/8] checking log
  ERROR: csum missing in log (root 5 inode 262 offset 5959680 address 0x5a000 length 1347584)
  ERROR: errors found in log
  [2/8] checking root items
  (...)

And in the log tree, the corresponding file extent item:

  $ btrfs inspect-internal dump-tree /dev/sdc
  (...)
        item 38 key (262 EXTENT_DATA 5959680) itemoff 1796 itemsize 53
                generation 11 type 1 (regular)
                extent data disk byte 0 nr 0
                extent data offset 368640 nr 1347584 ram 1716224
                extent compression 0 (none)
  (...)

This false alert happens because we sum the file extent item's offset to
its logical address before we attempt to skip holes at
check_range_csummed(), so we end up passing a non-zero logical address to
that function (0 + 368640), which will attempt to find a csum for that
invalid address and fail.

This type of error can be sporadically triggered by several test cases
from fstests such as btrfs/192 for example.

Fix this by skipping csum search if the file extent item's logical disk
address is 0 before summing the offset.

Fixes: 88dc309aca10 ("btrfs-progs: check: explicit holes in log tree don't get csummed")
Reviewed-by: Boris Burkov <boris@bur.io>
Reviewed-by: Qu Wenruo <wqu@suse.com>
Signed-off-by: Filipe Manana <fdmanana@suse.com>
Signed-off-by: David Sterba <dsterba@suse.com>

check/main.c
@@ -9694,10 +9694,6 @@ static int check_range_csummed(struct btrfs_root *root, u64 addr, u64 length)
 	u64 data_len;
 	int ret;
 
-	/* Explicit holes don't get csummed */
-	if (addr == 0)
-		return 0;
-
 	ret = btrfs_search_slot(NULL, root, &key, &path, 0, 0);
 	if (ret < 0)
 		return ret;
@@ -9807,12 +9803,15 @@ static int check_log_root(struct btrfs_root *root, struct cache_tree *root_cache
 			if (btrfs_file_extent_type(leaf, fi) != BTRFS_FILE_EXTENT_REG)
 				goto next;
 
+			addr = btrfs_file_extent_disk_bytenr(leaf, fi);
+			/* An explicit hole, skip as holes don't have csums. */
+			if (addr == 0)
+				goto next;
+
 			if (btrfs_file_extent_compression(leaf, fi)) {
-				addr = btrfs_file_extent_disk_bytenr(leaf, fi);
 				length = btrfs_file_extent_disk_num_bytes(leaf, fi);
 			} else {
-				addr = btrfs_file_extent_disk_bytenr(leaf, fi) +
-				       btrfs_file_extent_offset(leaf, fi);
+				addr += btrfs_file_extent_offset(leaf, fi);
 				length = btrfs_file_extent_num_bytes(leaf, fi);
 			}
 