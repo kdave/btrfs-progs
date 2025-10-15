bf4cb6b83a55a65932de6b8c395536bf69ea0e3e
Sweet Tea Dorminy <sweettea-kernel@dorminy.me>
2025-10-15 14:11:52 +0200

    btrfs-progs: add inode encryption contexts
    Recapitulates relevant parts of kernel change 'btrfs: add inode
    encryption contexts'.
    
    Add key for the encryption context.
    
    Signed-off-by: Sweet Tea Dorminy <sweettea-kernel@dorminy.me>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- kernel-shared/uapi/btrfs_tree.h
+++ kernel-shared/uapi/btrfs_tree.h
@@ -165,6 +165,9 @@
 #define BTRFS_VERITY_DESC_ITEM_KEY	36
 #define BTRFS_VERITY_MERKLE_ITEM_KEY	37
 
+/* Data of fscrypt context. */
+#define BTRFS_FSCRYPT_CTXT_ITEM_KEY	41
+
 #define BTRFS_ORPHAN_ITEM_KEY		48
 /* reserve 2-15 close to the inode for later flexibility */
 
@@ -425,6 +428,7 @@ static inline __u8 btrfs_dir_flags_to_ftype(__u8 flags)
 #define BTRFS_INODE_NOATIME		(1U << 9)
 #define BTRFS_INODE_DIRSYNC		(1U << 10)
 #define BTRFS_INODE_COMPRESS		(1U << 11)
+#define BTRFS_INODE_ENCRYPT		(1U << 12)
 
 #define BTRFS_INODE_ROOT_ITEM_INIT	(1U << 31)
 
@@ -441,6 +445,7 @@ static inline __u8 btrfs_dir_flags_to_ftype(__u8 flags)
 	 BTRFS_INODE_NOATIME |						\
 	 BTRFS_INODE_DIRSYNC |						\
 	 BTRFS_INODE_COMPRESS |						\
+	 BTRFS_INODE_ENCRYPT |						\
 	 BTRFS_INODE_ROOT_ITEM_INIT)
 
 #define BTRFS_INODE_RO_VERITY		(1U << 0)
