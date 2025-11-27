a2dce43cfa8cddee48e8cd99da48af0cde31d74c
Qu Wenruo <wqu@suse.com>
2025-11-27 18:03:16 +1030

    btrfs-progs: misc-tests: check if free space tree is enabled after mount
    For bs < ps cases, a mount will always enable free-space-tree due to the
    limitation of v1 space cache.
    
    Test case misc/057 will lead to false failure if the page size is larger
    than the default 4K sector size.
    
    Add an extra check on free-space-tree after fs population, and skip the
    test case if so.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- tests/misc-tests/057-btrfstune-free-space-tree/test.sh
+++ tests/misc-tests/057-btrfstune-free-space-tree/test.sh
@@ -17,6 +17,14 @@ run_check_mount_test_dev
 populate_fs
 run_check_umount_test_dev
 
+# Check if the fs has free space tree already. Currently bs < ps mount
+# will always enable free-space-tree (no support for v1 free space cache)
+if run_check_stdout "$TOP/btrfs" inspect-internal dump-super "$TEST_DEV" |\
+	grep -q "FREE_SPACE_TREE"; then
+	_not_run "free-space-tree is always enabled for page size $(getconf PAGESIZE)"
+fi
+
 run_check $SUDO_HELPER "$TOP/btrfstune" --convert-to-free-space-tree "$TEST_DEV"
+
 
 run_check "$TOP/btrfs" check "$TEST_DEV"
