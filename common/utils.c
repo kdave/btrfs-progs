kdave/btrfs-progs

commit f1115bd7fd1c94779107e36f68a3a89d4e48b20f

btrfs-progs: count devices without SEARCH_TREE ioctl in get_fs_info()
To read the information about filesystem and devices we use the FS_INFO
ioctl, the num_devices however does not include the seeding device. To
compensate, search_chunk_tree_for_fs_info() enumerates the devices and
counts again using the SEARCH_TREE ioctl, which is privileged.

Due to that the whole get_fs_info() fails and prevents some commands to
run properly, eg. 'btrfs fi show' on a mounted path.

Change the counting to manual iteration up to the max_id which we have
read from the FS_INFO ioctl.

Signed-off-by: David Sterba <dsterba@suse.com>

common/utils.c
@@ -120,75 +120,6 @@ int get_df(int fd, struct btrfs_ioctl_space_args **sargs_ret)
 	return 0;
 }
 
-
-static u64 find_max_device_id(struct btrfs_tree_search_args *args, int nr_items)
-{
-	struct btrfs_dev_item *dev_item;
-	char *buf = btrfs_tree_search_data(args, 0);
-
-	buf += (nr_items - 1) * (sizeof(struct btrfs_ioctl_search_header)
-				       + sizeof(struct btrfs_dev_item));
-	buf += sizeof(struct btrfs_ioctl_search_header);
-
-	dev_item = (struct btrfs_dev_item *)buf;
-
-	return btrfs_stack_device_id(dev_item);
-}
-
-static int search_chunk_tree_for_fs_info(int fd,
-				struct btrfs_ioctl_fs_info_args *fi_args)
-{
-	int ret;
-	int max_items;
-	u64 start_devid = 1;
-	struct btrfs_tree_search_args args;
-	struct btrfs_ioctl_search_key *sk;
-
-	fi_args->num_devices = 0;
-
-	max_items = BTRFS_SEARCH_ARGS_BUFSIZE
-	       / (sizeof(struct btrfs_ioctl_search_header)
-			       + sizeof(struct btrfs_dev_item));
-
-	memset(&args, 0, sizeof(args));
-	sk = btrfs_tree_search_sk(&args);
-	sk->tree_id = BTRFS_CHUNK_TREE_OBJECTID;
-	sk->min_objectid = BTRFS_DEV_ITEMS_OBJECTID;
-	sk->min_type = BTRFS_DEV_ITEM_KEY;
-	sk->max_objectid = BTRFS_DEV_ITEMS_OBJECTID;
-	sk->max_type = BTRFS_DEV_ITEM_KEY;
-	sk->min_transid = 0;
-	sk->max_transid = (u64)-1;
-	sk->nr_items = max_items;
-	sk->max_offset = (u64)-1;
-
-again:
-	sk->min_offset = start_devid;
-
-	ret = btrfs_tree_search_ioctl(fd, &args);
-	if (ret < 0)
-		return -errno;
-
-	fi_args->num_devices += (u64)sk->nr_items;
-
-	if (sk->nr_items == max_items) {
-		start_devid = find_max_device_id(&args, sk->nr_items) + 1;
-		goto again;
-	}
-
-	/* Get the latest max_id to stay consistent with the num_devices */
-	if (sk->nr_items == 0)
-		/*
-		 * last tree_search returns an empty buf, use the devid of
-		 * the last dev_item of the previous tree_search
-		 */
-		fi_args->max_id = start_devid - 1;
-	else
-		fi_args->max_id = find_max_device_id(&args, sk->nr_items);
-
-	return 0;
-}
-
 /*
  * For a given path, fill in the ioctl fs_ and info_ args.
  * If the path is a btrfs mountpoint, fill info for all devices.
@@ -258,6 +189,8 @@ int get_fs_info(const char *path, struct btrfs_ioctl_fs_info_args *fi_args,
 
 	/* fill in fi_args if not just a single device */
 	if (fi_args->num_devices != 1) {
+		u64 count = 0;
+
 		ret = ioctl(fd, BTRFS_IOC_FS_INFO, fi_args);
 		if (ret < 0) {
 			ret = -errno;
@@ -265,16 +198,22 @@ int get_fs_info(const char *path, struct btrfs_ioctl_fs_info_args *fi_args,
 		}
 
 		/*
-		 * The fs_args->num_devices does not include seed devices
+		 * The fs_args->num_devices does not include seed devices,
+		 * so we count them manually.
 		 */
-		ret = search_chunk_tree_for_fs_info(fd, fi_args);
-		if (ret)
-			goto out;
+		for (u64 devid = 1; devid <= fi_args->max_id; devid++) {
+			struct btrfs_ioctl_dev_info_args args = { .devid = devid };
 
-		/*
-		 * search_chunk_tree_for_fs_info() will lacks the devid 0
-		 * so manual probe for it here.
-		 */
+			ret = ioctl(fd, BTRFS_IOC_DEV_INFO, &args);
+			if (ret == 0)
+				count++;
+		}
+		if (count > fi_args->num_devices) {
+			printf("found more devices than fs_info\n");
+			fi_args->num_devices = count;
+		}
+
+		/* We did not count devid 0, do another probe. */
 		ret = device_get_info(fd, 0, &tmp);
 		if (!ret) {
 			fi_args->num_devices++;

commit 82451bec91e5213665f3c3429e5c2ccb3de4d919

btrfs-progs: do not output extra lines about mismatching device numbers
[BUG]
Fstests btrfs/218 fails with newer btrfs-progs:

btrfs/218 2s ... - output mismatch (see /home/adam/xfstests-dev/results//btrfs/218.out.bad)
    --- tests/btrfs/218.out	2024-11-24 18:04:01.137258508 +1030
    +++ /home/adam/xfstests-dev/results//btrfs/218.out.bad	2025-11-27 20:19:21.653781264 +1030
    @@ -2,6 +2,7 @@
     Label: none  uuid: <UUID>
     	Total devices <NUM> FS bytes used <SIZE>
     	devid <DEVID> size <SIZE> used <SIZE> path SCRATCH_DEV
    +found more devices than fs_info
     [SCRATCH_DEV].write_io_errs    0
     [SCRATCH_DEV].read_io_errs     0
     [SCRATCH_DEV].flush_io_errs    0
    ...
    (Run 'diff -u /home/adam/xfstests-dev/tests/btrfs/218.out /home/adam/xfstests-dev/results//btrfs/218.out.bad'  to see the entire diff)

[CAUSE]
Commit f1115bd7fd1c ("btrfs-progs: count devices without SEARCH_TREE
ioctl in get_fs_info()") added a new message which will be triggered
every time we're calling get_fs_info() on a fs with a seed device.

This message is not really necessary as we know this behavior already
and is doing device number counting to handle such situation.

[FIX]
Just remove the unnecessary message which is confusing and lead to the
above golden output mismatch.

Fixes: f1115bd7fd1c ("btrfs-progs: count devices without SEARCH_TREE ioctl in get_fs_info()")
Reviewed-by: Boris Burkov <boris@bur.io>
Signed-off-by: Qu Wenruo <wqu@suse.com>

common/utils.c
@@ -208,10 +208,8 @@ int get_fs_info(const char *path, struct btrfs_ioctl_fs_info_args *fi_args,
 			if (ret == 0)
 				count++;
 		}
-		if (count > fi_args->num_devices) {
-			printf("found more devices than fs_info\n");
+		if (count > fi_args->num_devices)
 			fi_args->num_devices = count;
-		}
 
 		/* We did not count devid 0, do another probe. */
 		ret = device_get_info(fd, 0, &tmp);