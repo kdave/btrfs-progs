kdave/btrfs-progs

commit 408e45feb325ba9a952dd86c3b9eea10b00b313d

btrfs-progs: dump-tree: add extra chunk type checks
There is a report in the mailing list where a seemingly confused end
user is passing a physical bytenr into "btrfs inspect dump-tree".

The best case is that bytenr has no corresponding chunk and will be
rejected early.

But if there is a chunk cover that bytenr, and that chunk is a data
chunk, "btrfs ins dump-tree" will mostly result a checksum mismatch,
e.g.:

  $ btrfs ins dump-tree -b 13631488 ~/test.img
  btrfs-progs v6.19
  checksum verify failed on 13631488 wanted 0xcdcdcdcd found 0xf9d338a0
  ERROR: failed to read tree block 13631488

Note that 13631488 is the logical bytenr of the first data chunk.

This can be confusing for end users, as they won't know if it's really
some corrupted tree blocks, or they are just passing wrong numbers.

Enhance this by introducing a new check_metadata_logical() helper, to
make sure a chunk exists and the type is also correct.

Now the error would be more obvious to end users:

  $ ./btrfs ins dump-tree -b 13631488 ~/test.img
  btrfs-progs v6.19
  ERROR: logical 13631488 is not in a metadata chunk, found chunk 13631488 len 8388608 flags 0x1

Pull-request: #1092
Link: https://lore.kernel.org/linux-btrfs/77535c20-da3e-4dfb-b3d7-9426c25b5da3@free.fr/
Signed-off-by: Qu Wenruo <wqu@suse.com>
Signed-off-by: David Sterba <dsterba@suse.com>

cmds/inspect-dump-tree.c
@@ -33,6 +33,7 @@
 #include "kernel-shared/print-tree.h"
 #include "kernel-shared/extent_io.h"
 #include "kernel-shared/tree-checker.h"
+#include "kernel-shared/volumes.h"
 #include "common/defs.h"
 #include "common/extent-cache.h"
 #include "common/messages.h"
@@ -194,6 +195,25 @@ static int dump_add_tree_block(struct cache_tree *tree, u64 bytenr)
 	return ret;
 }
 
+static int check_metadata_logical(struct btrfs_fs_info *fs_info, u64 logical)
+{
+	struct cache_extent *ce;
+	struct map_lookup *map;
+
+	ce = search_cache_extent(&fs_info->mapping_tree.cache_tree, logical);
+	if (!ce || ce->start > logical) {
+		error("no chunk map found for logical %llu", logical);
+		return -ENOENT;
+	}
+	map = container_of(ce, struct map_lookup, ce);
+	if (!(map->type & BTRFS_BLOCK_GROUP_METADATA)) {
+		error("logical %llu is not in a metadata chunk, found chunk %llu len %llu flags 0x%llx",
+		      logical, ce->start, ce->size, map->type);
+		return -EINVAL;
+	}
+	return 0;
+}
+
 /*
  * Print all tree blocks recorded.
  * All tree block bytenr record will also be freed in this function.
@@ -228,6 +248,9 @@ static int dump_print_tree_blocks(struct btrfs_fs_info *fs_info,
 			goto next;
 		}
 
+		ret = check_metadata_logical(fs_info, bytenr);
+		if (ret < 0)
+			goto next;
 		eb = read_tree_block(fs_info, bytenr, &check);
 		if (!extent_buffer_uptodate(eb)) {
 			error("failed to read tree block %llu", bytenr);

commit 17c05936c9c088236968768d1869decc020bc3d7

btrfs-progs: misc-tests: add a regression test case for dump tree
The new test case will:

- Create an empty btrfs
- Grab the chunk tree root bytenr
- Pass that bytenr into "btrfs ins dump-tree -b <bytenr>"
  That must success.

This is for the regression introduced by commit 408e45feb325
("btrfs-progs: dump-tree: add extra chunk type checks").

Signed-off-by: Qu Wenruo <wqu@suse.com>

tests/misc-tests/072-dump-tree-chunk-bytenr/test.sh
@@ -1,0 +1,25 @@
+#!/bin/bash
+# Test "btrfs ins dump-tree -b <bytenr>" when the bytenr belongs to a chunk
+# tree block.
+#
+# This is a regression test for commit 408e45feb325 ("btrfs-progs:
+# dump-tree: add extra chunk type checks"), which is too strict and
+# rejects chunk tree blocks.
+
+source "$TEST_TOP/common" || exit
+source "$TEST_TOP/common.convert" || exit
+
+check_prereq mkfs.btrfs
+check_prereq btrfs
+
+setup_root_helper
+prepare_test_dev
+
+run_check_mkfs_test_dev
+chunk_root=$(run_check_stdout "$TOP/btrfs" inspect-internal dump-tree -t chunk "$TEST_DEV" |\
+	     grep "owner CHUNK_TREE" | cut -f 2 -d\ )
+if [ -z "$chunk_root" ]; then
+	_fail "unable to get the chunk root bytenr"
+fi
+
+run_check "$TOP/btrfs" inspect-internal dump-tree -b "$chunk_root" "$TEST_DEV"