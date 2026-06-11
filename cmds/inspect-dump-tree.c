kdave/btrfs-progs

commit 408e45feb325ba9a952dd86c3b9eea10b00b313d

btrfs-progs: dump-tree: add extra chunk type checks
There is a report in the mailing list where a seemingly confused end
user is passing a physical bytenr into "btrfs inspect dump-tree".

The best case is that bytenr has no corresponding chunk and will be
rejected early.

But if there is a chunk cover that bytenr, and that chunk is a data
chunk, "btrfs ins dump-tree" will mostly result a checksum mismatch,
e.g.:

  $ btrfs ins dump-tree -b 13631488 ~/test.img
  btrfs-progs v6.19
  checksum verify failed on 13631488 wanted 0xcdcdcdcd found 0xf9d338a0
  ERROR: failed to read tree block 13631488

Note that 13631488 is the logical bytenr of the first data chunk.

This can be confusing for end users, as they won't know if it's really
some corrupted tree blocks, or they are just passing wrong numbers.

Enhance this by introducing a new check_metadata_logical() helper, to
make sure a chunk exists and the type is also correct.

Now the error would be more obvious to end users:

  $ ./btrfs ins dump-tree -b 13631488 ~/test.img
  btrfs-progs v6.19
  ERROR: logical 13631488 is not in a metadata chunk, found chunk 13631488 len 8388608 flags 0x1

Pull-request: #1092
Link: https://lore.kernel.org/linux-btrfs/77535c20-da3e-4dfb-b3d7-9426c25b5da3@free.fr/
Signed-off-by: Qu Wenruo <wqu@suse.com>
Signed-off-by: David Sterba <dsterba@suse.com>

cmds/inspect-dump-tree.c
@@ -33,6 +33,7 @@
 #include "kernel-shared/print-tree.h"
 #include "kernel-shared/extent_io.h"
 #include "kernel-shared/tree-checker.h"
+#include "kernel-shared/volumes.h"
 #include "common/defs.h"
 #include "common/extent-cache.h"
 #include "common/messages.h"
@@ -194,6 +195,25 @@ static int dump_add_tree_block(struct cache_tree *tree, u64 bytenr)
 	return ret;
 }
 
+static int check_metadata_logical(struct btrfs_fs_info *fs_info, u64 logical)
+{
+	struct cache_extent *ce;
+	struct map_lookup *map;
+
+	ce = search_cache_extent(&fs_info->mapping_tree.cache_tree, logical);
+	if (!ce || ce->start > logical) {
+		error("no chunk map found for logical %llu", logical);
+		return -ENOENT;
+	}
+	map = container_of(ce, struct map_lookup, ce);
+	if (!(map->type & BTRFS_BLOCK_GROUP_METADATA)) {
+		error("logical %llu is not in a metadata chunk, found chunk %llu len %llu flags 0x%llx",
+		      logical, ce->start, ce->size, map->type);
+		return -EINVAL;
+	}
+	return 0;
+}
+
 /*
  * Print all tree blocks recorded.
  * All tree block bytenr record will also be freed in this function.
@@ -228,6 +248,9 @@ static int dump_print_tree_blocks(struct btrfs_fs_info *fs_info,
 			goto next;
 		}
 
+		ret = check_metadata_logical(fs_info, bytenr);
+		if (ret < 0)
+			goto next;
 		eb = read_tree_block(fs_info, bytenr, &check);
 		if (!extent_buffer_uptodate(eb)) {
 			error("failed to read tree block %llu", bytenr);

commit 2d2b0c0050c59e76b8c70caf7e26d5b2c2ae638a

btrfs-progs: dump-tree: fix the chunk type check for system chunks
[BUG]
Commit 408e45feb325 ("btrfs-progs: dump-tree: add extra chunk type
checks") introduced one extra check on when using "btrfs ins dump-tree
-b <bytenr>" option, to prevent end users from passing logical address
belonging to data chunks.

However that check forget that we also have metadata in system chunks,
thus will cause the following false alerts:

 # mkfs.btrfs -f $dev
 # btrfs ins dump-tree -t chunk $dev | grep "owner CHUNK_TREE"
 leaf 22036480 items 4 free space 15781 generation 8 owner CHUNK_TREE
 ^^^ This means we can still print the chunk tree

 # btrfs ins dump-tree -b 22036480 $dev
 btrfs-progs v6.19.1
 ERROR: logical 22036480 is not in a metadata chunk, found chunk 22020096 len 8388608 flags 0x22

[CAUSE]
I'm an idiot and forgot to check system chunks.

[FIX]
Add system chunk type into the check.

Fixes: 408e45feb325 ("btrfs-progs: dump-tree: add extra chunk type checks")
Signed-off-by: Qu Wenruo <wqu@suse.com>

cmds/inspect-dump-tree.c
@@ -206,8 +206,9 @@ static int check_metadata_logical(struct btrfs_fs_info *fs_info, u64 logical)
 		return -ENOENT;
 	}
 	map = container_of(ce, struct map_lookup, ce);
-	if (!(map->type & BTRFS_BLOCK_GROUP_METADATA)) {
-		error("logical %llu is not in a metadata chunk, found chunk %llu len %llu flags 0x%llx",
+	if (!(map->type & (BTRFS_BLOCK_GROUP_METADATA |
+			   BTRFS_BLOCK_GROUP_SYSTEM))) {
+		error("logical %llu is not in a metadata/system chunk, found chunk %llu len %llu flags 0x%llx",
 		      logical, ce->start, ce->size, map->type);
 		return -EINVAL;
 	}